7826342e88e0c2466bb42e45c2bb27bc72119b90
soriphoono <soriphoono@gmail.com>
2023-09-08 17:28:55 -0500

    Updated terminal creation, loaded primary target from disk

--- .config/yadm/installer/src/ui/mod.rs
+++ .config/yadm/installer/src/ui/mod.rs
@@ -1,6 +1,7 @@
 use std::{
-    error, fs,
-    io::{stdout, Stdout},
+    error,
+    fs::{self, File},
+    io::{self, stdout, BufReader, Stdout},
     path::{self, Path, PathBuf},
     str::FromStr,
 };
@@ -12,52 +13,43 @@ use crate::{
     app,
     config::{
         system::{Backup, ConfigFile, Package},
-        BuildTarget, InstallTarget,
+        BuildTarget, InstallTarget, HOME,
     },
 };
 
 type Console = Terminal<CrosstermBackend<Stdout>>;
 
-fn setup_terminal() -> Result<Console, Box<dyn error::Error>> {
+fn setup_terminal() -> Result<Console, io::Error> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     enable_raw_mode()?;
 
-    terminal.clear();
-    terminal.hide_cursor();
+    terminal.clear()?;
+    terminal.hide_cursor()?;
 
     Ok(terminal)
 }
 
 fn default_target(terminal: &Console) -> BuildTarget {
-    let pacman_backup = "/etc/pacman.conf";
+    match serde_json::from_reader(BufReader::new(
+        match File::open(&format!("{}/.config/yadm/targets/primary.json", HOME)) {
+            Ok(file) => file,
+            Err(why) => {
+                log::error!("Failed to open primary target file: {}", why);
 
-    let config_files = if Path::new(&format!("{}.bak", pacman_backup)).exists() {
-        log::info!("Found previous backup for pacman.conf");
+                panic!("Failed to open primary target file, check logs above! Run yadm restore to fix this issue.");
+            }
+        },
+    )) {
+        Ok(target) => target,
+        Err(why) => {
+            log::error!("Failed to parse primary target file: {}", why);
 
-        vec![
-            ConfigFile::new(pacman_backup).backup(Backup::Restore),
-            ConfigFile::new("/etc/xdg/reflector/reflector.conf").backup(Backup::Restore),
-        ]
-    } else {
-        log::debug!("Failed to find previous backup for pacman.conf");
-
-        vec![]
-    };
-
-    BuildTarget {
-        depends: vec!["system/cli".to_string()],
-
-        optional_repos: vec![],
-
-        packages: vec![Package::system("reflector"), Package::system("")],
-        config_files,
-        services: vec![],
+            panic!("Failed to parse primary target file, check logs above! Run yadm restore to fix this issue.");
+        }
     }
 }
 
 pub fn aquire_target() -> Result<InstallTarget, Box<dyn error::Error>> {
-    const TARGET_PATH: &str = "primary.json";
-
     let terminal = setup_terminal()?;
 
     Ok(InstallTarget::new(default_target(&terminal))?)
