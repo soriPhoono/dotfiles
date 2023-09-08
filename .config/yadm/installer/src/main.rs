f49b0a3e6250c4a2bcc47077e288b708e7bcc927
soriphoono <soriphoono@gmail.com>
2023-09-08 05:32:41 -0500

    Starting implementation of cli requested operations

--- .config/yadm/installer/src/main.rs
+++ .config/yadm/installer/src/main.rs
@@ -1,8 +1,13 @@
-use std::{error::Error, fs::File, io::BufWriter, path::Path};
+use std::{error::Error, fs::File, io::BufWriter};
 
-use crate::config::{
-    system::{Backup, ConfigFile, Package, Repository},
-    BuildTarget, InstallTarget,
+use clap::Parser;
+
+use crate::{
+    app::Commands,
+    config::{
+        system::{Backup, ConfigFile, Package, Repository},
+        BuildTarget, InstallTarget,
+    },
 };
 
 mod app;
@@ -10,38 +15,20 @@ mod config;
 mod ui;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // General setup
     human_panic::setup_panic!();
     env_logger::init();
 
-    let target = BuildTarget {
-        depends: vec![],
-
-        optional_repos: vec![
-            Repository::new("chaotic-aur")
-                .bootstrap_command("sudo sed -i 's/#\\[multilib\\]/\\[multilib\\]/' /etc/pacman.conf".to_string())
-                .bootstrap_command("sudo sed -i '/\\[multilib\\]/!b;n;cInclude = \\/etc\\/pacman.d\\/mirrorlist' /etc/pacman.conf".to_string())
-        ],
-
-        packages: vec![
-            Package::system("base-devel")
-                .with_depend(Package::system("base"))
-                .with_depend(Package::system("git"))
-        ],
-        config_files: vec![
-            ConfigFile::new("/etc/pacman.conf")
-                .backup(Backup::move_to())
-        ],
-        services: vec![],
-
-        commands: vec![],
-    };
-
-    let writer = BufWriter::new(File::create("test.json")?);
-    serde_json::to_writer_pretty(writer, &target)?;
-
-    let install_target = InstallTarget::new(target)?;
-
-    println!("{}", install_target);
+    // Parse command line arguments
+    let args = app::Cli::parse();
+    match args.subcommand {
+        Commands::Install { target } => {
+            if let Some(target) = target {
+            } else {
+                // spawn user interface to get target config
+            }
+        }
+    }
 
     Ok(())
 }
