48adbb0527a015d9d3b0565dd2d8e1f07176e4ff
soriphoono <soriphoono@gmail.com>
2023-09-09 15:32:29 -0500

    Updated widget library with test widgets, created Label widget

--- .config/yadm/installer/ui/widgets.py
+++ .config/yadm/installer/ui/widgets.py
@@ -1,4 +1,5 @@
 import abc
+import curses
 
 import windows
 
@@ -24,7 +25,7 @@ class Widget(abc.ABC):
 
     def set_y(self, y: int) -> None:
         self.y = y
-    
+
     def set_location(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
@@ -41,14 +42,75 @@ class Widget(abc.ABC):
 
     def visible(self) -> bool:
         self.visible = not self.visible
-    
+
     def is_visible(self) -> bool:
         return self.visible
 
     @abc.abstractmethod
-    def draw(self) -> None:
-        pass
+    def draw(self, stdscr: curses.window) -> None:
+        if self.border:
+            stdscr.addstr(self.y, self.x, '┌' + '─' * (self.width - 2) + '┐')
+            for i in range(self.height - 2):
+                stdscr.addstr(self.y + i + 1, self.x, '│' +
+                              ' ' * (self.width - 2) + '│')
+            stdscr.addstr(self.y + self.height - 1, self.x,
+                          '└' + '─' * (self.width - 2) + '┘')
 
     @abc.abstractmethod
     def update(self, key: int) -> None:
         pass
+
+
+class Label(Widget):
+    def __init__(self, x: int, y: int, width: int, height: int, text: str, border: bool = False) -> None:
+        super().__init__(x, y, width, height, border)
+
+        self.text: str = text
+
+    def draw(self, stdscr: curses.window) -> None:
+        super().draw(stdscr)
+
+        stdscr.addstr(self.y + self.height // 2, self.x +
+                      self.width // 2 - len(self.text) // 2, self.text)
+
+    def update(self, key: int) -> None:
+        pass
+
+
+class Button(Widget):
+    def __init__(self, x: int, y: int, width: int, height: int, text: str, border: bool = False) -> None:
+        super().__init__(x, y, width, height, border)
+
+        self.text: str = text
+        self.is_selected: bool = False
+
+    def draw(self, stdscr: curses.window) -> None:
+        super().draw(stdscr)
+
+        stdscr.addstr(self.y + self.height // 2, self.x +
+                      self.width // 2 - len(self.text) // 2, self.text)
+        
+    def update(self, key: int) -> None:
+        pass
+
+class Input(Widget):
+    def __init__(self, x: int, y: int, width: int, height: int, hint_text: str, border: bool = False, on_return: callable = None) -> None:
+        super().__init__(x, y, width, height, border)
+
+        self.hint_text: str = hint_text
+
+        self.on_return: callable = on_return
+
+    def on_return(self, on_return: callable = None) -> None:
+        if on_return:
+            self.on_return = on_return
+
+    def draw(self, stdscr: curses.window) -> None:
+        super().draw(stdscr)
+
+        stdscr.addstr(self.y + self.height // 2, self.x +
+                      self.width // 2 - len(self.hint_text) // 2, self.hint_text)
+
+    def update(self, key: int) -> None:
+        if key == curses.KEY_ENTER:
+            self.on_return()
