6d67f6c82a856cc008d9cebedb9b9e505d278c62
soriphoono <soriphoono@gmail.com>
2023-09-18 18:49:38 -0500

    Began adding advanced anonymity features

--- .config/yadm/cli/networking.sh
+++ .config/yadm/cli/networking.sh
@@ -9,4 +9,7 @@ packages=(
 
 paru -S --noconfirm --needed "${packages[@]}" >>/dev/null 2>&1
 
-
+read -p "Enable NetworkManager advanced features (tor, i2p, dnscrypt proxy)? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    
+fi
