d8f236d9796eee351b22e6f31ae5e6171d37841d
soriphoono <soriphoono@gmail.com>
2023-09-19 13:00:40 -0500

    Fixed bug regarding commas being in bash array printing

--- .config/yadm/personal/developer_environment.sh
+++ .config/yadm/personal/developer_environment.sh
@@ -5,59 +5,60 @@
 # Setup development tools
 
 languages=(
-    "C/C++",
-    "Zig",
-    "Rust",
-    "Go",
-    "Java",
-    "Perl",
-    "Python",
-    "Lua",
-    "HTML/CSS/JS",
+    "C/C++"
+    "Zig"
+    "Rust"
+    "Go"
+    "Java"
+    "Perl"
+    "Python"
+    "Lua"
+    "HTML/CSS/JS"
 )
 
 packages=()
 
 i=1
 echo "Available languages:"
-for language in languages; do
+for language in $languages; do
     echo "    $i: $language"
+    i=$((i + 1))
 done
 read -p "Select languages: " languages
-for language in languages; do
+for language in $languages; do
     case $language in
     1)
-        packages+=("valgrind", "cmake", "ninja", "meson", "gdb", "gcc", "lldb", "clang", "llvm", "lld")
+        packages+=("valgrind" "cmake" "ninja" "meson" "gdb" "gcc" "lldb" "clang" "llvm" "lld")
         ;;
     2)
-        packages+=("zls", "zig")
+        packages+=("zls" "zig")
         ;;
     3)
         packages+=("rust")
         ;;
     4)
-        packages+=("go", "delve", "gopls")
+        packages+=("go" "delve" "gopls")
         ;;
     5)
-        packages+=("jdk-openjdk", "jdk17-openjdk", "jdk11-openjdk", "jdk8-openjdk")
+        packages+=("jdk-openjdk" "jdk17-openjdk" "jdk11-openjdk" "jdk8-openjdk")
         ;;
     6)
         packages+=("perl")
         ;;
     7)
-        packages+=("python", "python-pip")
+        packages+=("python" "python-pip")
 
         read -p "Install django? [Y/n] " -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            packages+=("python-django", "python-mysqlclient")
+            packages+=("python-django" "python-mysqlclient")
         fi
         ;;
     8)
-        packages+=("luarocks", "luajit", "lua")
+        packages+=("luarocks" "luajit" "lua")
         ;;
     9)
-        packages+=("nodejs", "npm")
+        packages+=("nodejs" "npm")
         ;;
     esac
 done
@@ -77,7 +78,7 @@ fi
 read -p "Install visual studio code and obsidian notation software? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("visual-studio-code-bin", "obsidian")
+    packages+=("visual-studio-code-bin" "obsidian")
 fi
 
 read -p "Install imhex hex viewer? [Y/n] " -n 1 -r
@@ -90,7 +91,7 @@ fi
 read -p "Install virtualbox and extension pack? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("virtualbox", "virtualbox-host-dkms", "virtualbox-guest-iso", "virtualbox-unattended-templates", "virtualbox-ext-oracle")
+    packages+=("virtualbox" "virtualbox-host-dkms" "virtualbox-guest-iso" "virtualbox-unattended-templates" "virtualbox-ext-oracle")
 fi
 
 sudo usermod -aG vboxusers "$USER"
@@ -99,7 +100,7 @@ sudo usermod -aG vboxusers "$USER"
 read -p "Install docker and docker-compose? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("docker", "docker-compose")
+    packages+=("docker" "docker-compose")
 fi
 
 sudo systemctl enable --now docker.service
