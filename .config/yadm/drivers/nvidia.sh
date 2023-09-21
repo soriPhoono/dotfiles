ab4d0d42d057efd5cd76489b4f148a388d01b144
soriphoono <soriphoono@gmail.com>
2023-09-21 07:31:28 -0500

    Added nvidia driver initialization

---
+++ .config/yadm/drivers/nvidia.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Path:         ~/.config/yadm/drivers/nvidia.sh
+# Description:  Driver installation script for Nvidia graphics cards
+# Author:       Sori Phoono <soriphoono@gmail.com>
+
+MULTILIB=false
+for arg in "$@"; do
+    case "$arg" in
+    -m | --multilib)
+        MULTILIB=true
+        ;;
+    esac
+done
+
+packages=(
+    "nvidia-dkms"
+    "nvidia-utils"
+    "opencl-nvidia"
+    "prime-run"
+)
+
+commands=(
+    "sudo sed -i \"kms //g\" /etc/mkinitcpio.conf"
+    "sudo mkinitcpio -P"
+    "sudo sed -i '/^options/ s/$/ nvidia-drm.modeset=1/' /boot/loader/entries/*.conf"
+    "sudo modprobe nvidia_uvm"
+)
+
+if $MULTILIB; then
+    packages+=("lib32-nvidia-utils")
+fi
+
+# Install packages
+sudo pacman -S "${packages[@]}"
+
+# Execute commands
+for command in "${commands[@]}"; do
+    eval "$command"
+done
