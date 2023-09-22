02400bd0b1ca4b87cc99e2742f68ffb3a02cde13
soriphoono <soriphoono@gmail.com>
2023-09-22 10:39:41 -0500

    Added authorship header and package notes

--- .config/yadm/personal/recording_tools.sh
+++ .config/yadm/personal/recording_tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Path:        ~/.dotfiles/personal/recording_tools.sh
+# Description: Bootstrap script for recording tools
+# Author:      Sori Phoono <soriphoono@gmail.com>
 
 MULTILIB=false
 for arg in "$@"; do
@@ -11,16 +13,20 @@ for arg in "$@"; do
     esac
 done
 
-packages=("obs-gstreamer" "obs-vkcapture-git" "obs-backgroundremoval")
+packages=(
+    "obs-gstreamer"         # OBS Studio gstreamer plugin for gstreamer based hardware accellerated encoding/decoding (AUR)
+    "obs-vkcapture-git"     # OBS Studio Vulkan capture plugin (AUR)
+    "obs-backgroundremoval" # OBS Studio background removal plugin (AUR)
+)
 
 if grep -q "AMD" <<<"$(lspci | grep -e \"VGA\" -e \"3D\")" && grep -q "amf-amdgpu-pro" <<<"$(paru -Q)"; then
-    packages+=("obs-studio-amf")
+    packages+=("obs-studio-amf") # OBS Studio AMD media framework plugin (AUR)
 else
-    packages+=("obs-studio-git")
+    packages+=("obs-studio-git") # OBS Studio (AUR)
 fi
 
 if $MULTILIB; then
-    packages+=("lib32-obs-vkcapture-git")
+    packages+=("lib32-obs-vkcapture-git") # OBS Studio Vulkan capture plugin (32-bit) (AUR)
 fi
 
 echo "Installing packages..."
