c6445160123a20ac0648970ad87cf1912b41f475
soriphoono <soriphoono@gmail.com>
2023-09-22 10:34:38 -0500

    Added authorship header and package notes for maintainers and installers

--- .config/yadm/personal/gaming.sh
+++ .config/yadm/personal/gaming.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Path:         ~/.config/yadm/personal/gaming.sh
+# Description:  Bootstrap script for gaming environment
+# Author:       Sori Phoono (soriphoono@gmail.com)
 
 MULTILIB=false
 for arg in "$@"; do
@@ -13,48 +15,65 @@ done
 
 # Install core packages for gaming environment
 packages=(
-    "soundfont-fluid"
-    "boxtron"
-    "protonup-git"
-    "protontricks-git"
-    "wine"
-    "winetricks"
-    "gamemode"
-    "mangohud"
-    "replay-sorcery"
-    "dxvk-async-git"
-    "vkd3d"
-    "gamescope"
+    "soundfont-fluid"  # FluidR3 soundfont
+    "boxtron"          # DOSBox fork for Steam Play compatibility
+    "protonup-git"     # Proton updater
+    "protontricks-git" # Proton tricks (configuration tool)
+    "wine"             # Wine
+    "winetricks"       # Wine tricks (configuration tool)
+    "gamemode"         # Game mode
+    "mangohud"         # MangoHud (game overlay)
+    "replay-sorcery"   # Replay sorcery (game replay tool)
+    "dxvk-async-git"   # DXVK async (DirectX to Vulkan translation layer)
+    "vkd3d"            # VKD3D (DirectX 12 to Vulkan translation layer)
+    "gamescope"        # Gamescope (game compositor) (for fixing issues in window manager on wayland based compositors)
 )
 
 # Install proton and it's dependencies
 if $MULTILIB; then
-    packages+=("lib32-gamemode" "lib32-vkd3d")
+    packages+=(
+        "lib32-gamemode" # Game mode (32-bit)
+        "lib32-vkd3d"    # VKD3D (32-bit)
+    )
 fi
 
 # Install steam(-native) and it's dependencies
-read -p "Install steam-native-runtime? [Y/n] " -n 1 -r
+read -p "Install steam? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("steam" "steam-native-runtime")
+    if $MULTILIB; then
+        packages+=(
+            "steam"                # Steam
+            "steam-native-runtime" # Steam native runtime
+        )
+    else
+        echo "Steam requires multilib to be enabled."
+    fi
 fi
 
 read -p "Install heroic-launcher? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("heroic-games-launcher-bin")
+    packages+=(
+        "heroic-games-launcher-bin" # Heroic games launcher
+    )
 fi
 
 read -p "Install minecraft launcher? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("prismlauncher")
+    packages+=(
+        "prismlauncher" # Minecraft instance and version manager
+    )
 fi
 
 read -p "Install tales of maj'eyal? [Y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    packages+=("tome4-bin" "tome4-zomnibus_addon")
+    packages+=(
+        "tome4-bin"            # Tales of Maj'Eyal
+        "tome4-zomnibus_addon" # Tales of Maj'Eyal Zomnibus addon (optional qol mods to improve experience while preserving vanilla feel)
+    )
 fi
 
 echo "Installing packages..."
