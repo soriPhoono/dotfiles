8257a3149d268b7f4e7e8acd8f67d66e972d81df
soriphoono <soriphoono@gmail.com>
2023-09-25 18:11:35 -0500

    added libre office

--- .config/yadm/personal/office.sh
+++ .config/yadm/personal/office.sh
@@ -6,5 +6,17 @@
 
 packages=(
     "libreoffice-fresh"
-    
+    "libreoffice-extension-texmaths"
+    "libreoffice-extension-writer2latex"
+    "libreoffice-extension-languagetool"
+    "hunspell"
+    "hunspell-en_us"
+    "hyphen"
+    "hyphen-en"
+    "libmythes"
+    "mythes-en"
 )
+
+echo "Installing libreoffice..."
+paru -S --noconfirm --needed ${packages[@]} >/dev/null
+echo "Done!"
