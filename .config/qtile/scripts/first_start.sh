bf3be5232f732ee0aed5bbff1d97bdc66d7567d8
soriphoono <soriphoono@gmail.com>
2023-09-25 14:18:18 -0500

    organized first_start and made gtk settings commands for easy access

--- .config/qtile/scripts/first_start.sh
+++ .config/qtile/scripts/first_start.sh
@@ -2,12 +2,15 @@
 
 xhost + local &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & # For authentication
-
 redshift &
-picom &
 
-unclutter &
+picom &
 dunst &
+unclutter &
+
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice' &
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &
+gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Teal-dark' &
 
 ~/.local/bin/notifications/packages.sh &
 
