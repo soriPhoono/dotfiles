0d236bf0f414e93fdb8f1d85d39f12389ef489c6
soriphoono <soriphoono@gmail.com>
2023-09-25 14:29:22 -0500

    updated qtile configuration

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -24,21 +24,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import math
+from os import path
+from subprocess import call
+
+
 from libqtile import layout
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Group, Key, Screen
 from libqtile.lazy import lazy
 from libqtile.hook import subscribe
 
-import os
-import math
-from subprocess import call
-
-import theme
+from wm import theme, dispatchers
 
 home = os.path.expanduser('~')
 monitor_count = 3
-
-mod = "mod4"
 
 terminal = os.getenv("TERM") or "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
@@ -47,72 +46,7 @@ editor_cmd = terminal + " -e " + editor
 browser = os.getenv("BROWSER") or "firefox"
 
 # TODO: adjust hotkeys
-keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Control system
-    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod], "p", lazy.spawn(home + "/.config/rofi/powermenu/type-6/powermenu.sh"),
-        desc="Launch rofi powermenu"),
-    # Switch between windows
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "Left", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "Right", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "control"], "n", lazy.layout.normalize(),
-        desc="Reset all window sizes"),
-    Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle fullscreen"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "a", lazy.spawn(
-        home + "/.config/rofi/launchers/type-6/launcher.sh"), desc="Launch rofi"),
-    Key([mod], "e", lazy.spawn("pcmanfm"), desc="Launch file manager"),
-    Key([mod], "F4", lazy.spawn("flameshot gui"), desc="Launch xkill"),
-
-    # Volume control
-    Key([], "XF86AudioRaiseVolume", lazy.spawn(
-        f"pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Increase volume"),
-    Key([], "XF86AudioLowerVolume", lazy.spawn(
-        f"pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Decrease volume"),
-    Key([], "XF86AudioMute", lazy.spawn(
-        f"pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Mute volume"),
-    Key([], "XF86AudioMicMute", lazy.spawn(
-        f"pactl set-source-mute @DEFAULT_SOURCE@ toggle"), desc="Mute microphone"),
-    Key([], "XF86AudioPlay", lazy.spawn(
-        "playerctl play-pause"), desc="Play/Pause"),
-    Key([], "XF86AudioNext", lazy.spawn("playerctl next"), desc="Next track"),
-    Key([], "XF86AudioPrev", lazy.spawn(
-        "playerctl previous"), desc="Previous track"),
-
-    # Application launchers
-    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
-    Key([mod], "c", lazy.spawn("code"), desc="Launch VSCode"),
-    Key([mod], "d", lazy.spawn("discord --start-minimized"), desc="Launch Discord"),
-    Key([mod], "m", lazy.spawn("exaile"), desc="Launch Spotify"),
-]
+keys = dispatchers.keys
 
 screens = [
     Screen(
