8bbbf844704bb08f6e4adf9009e041c53d67fff3
soriphoono <soriphoono@gmail.com>
2023-09-25 10:33:25 -0500

    corrected stdout routing and execution chain

--- .config/yadm/personal/developer_environment.sh
+++ .config/yadm/personal/developer_environment.sh
@@ -55,7 +55,7 @@ for language in $languages; do
             "rustup" # Rust toolchain manager
         )
         commands+=(
-            "rustup default stable" # Set default rust toolchain to stable (download and configure it)
+            "rustup default stable >/dev/null" # Set default rust toolchain to stable (download and configure it)
         )
         ;;
     "4")
@@ -117,9 +117,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     packages+=(
         "github-cli" # Github cli
     )
-fi
-if ! gh auth status; then
-    gh auth login
+
+    if ! gh auth status; then
+        gh auth login
+    fi
 fi
 
 # Text Editors/Markdown writers
@@ -151,9 +152,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         "virtualbox-unattended-templates" # Virtualbox unattended templates
         "virtualbox-ext-oracle"           # Virtualbox extension pack
     )
-fi
 
-sudo usermod -aG vboxusers "$USER" /dev/null 
+    commands+=("sudo usermod -aG vboxusers "$USER" >/dev/null")
+fi
 
 # Docker
 read -p "Install docker and docker-compose? [Y/n] " -n 1 -r
@@ -163,16 +164,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         "docker"         # Docker containerized application system
         "docker-compose" # Docker compose (docker container manager)
     )
+
+    commands+=("sudo systemctl enable --now docker.service >/dev/null")
 fi
 
-sudo systemctl enable --now docker.service /dev/null 
-
 echo "Installing packages..."
-paru -S --noconfirm --needed "${packages[@]}" /dev/null 
-echo "Finished installing developer environment packages"
-
-echo "Running setup commands..."
+paru -S --noconfirm --needed "${packages[@]}" >/dev/null
 for command in "${commands[@]}"; do
-    eval $command /dev/null 
+    eval $command >/dev/null
 done
-echo "Finished running setup commands"
+echo "Finished installing developer environment packages"
