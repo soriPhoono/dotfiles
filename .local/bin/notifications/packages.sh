256405a414213eebd354e8cbfd926a699737fab8
soriphoono <soriphoono@gmail.com>
2023-10-05 17:46:42 -0500

    Fixed repeated update count bug

--- .local/bin/notifications/packages.sh
+++ .local/bin/notifications/packages.sh
@@ -2,8 +2,8 @@
 
 source "$HOME"/.local/bin/environment.sh
 
-num_updates=$(checkupdates | wc -l)
 while true; do
+    num_updates=$(checkupdates | wc -l)
     if [[ $num_updates -gt 0 ]]; then
         notify-send -i "$icon_package" -u normal -r 60 "Available updates" "$num_updates package(s) require updating."
     fi
