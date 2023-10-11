1725606c58580c0064a4d0af2e78dffd8b43be74
soriphoono <soriphoono@gmail.com>
2023-10-11 16:45:35 -0500

    Updated nix configuration

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -2,15 +2,50 @@
 
 {
   include = [
-    ./hardware.nix # TODO: correct file name
+    ./hardware-configuration.nix # TODO: correct file name
   ];
 
-  boot.loader = {
-    systemd-boot = {
-      enabled = true;
-      consoleMode = "max";
+  boot = {
+    loader = {
+      systemd-boot = {
+        enabled = true;
+        consoleMode = "max";
+      };
+      efi.canTouchEfiVariables = true;
     };
+    cleanTmpDir = true;
   };
 
-  
+  networking = {
+    hostName = "home-server";
+    networkmanager.enable = true;
+    wireless.enable = true;
+  };
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "America/Chicago";
+
+  programs = {
+    zsh.enable = true;
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    tree
+    curl
+    git
+    gnupg
+    neovim
+    tmux
+    gnumake
+    unzip
+  ];
+
+  system.stateVersion = "23.05";
 }
