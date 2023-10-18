1f20fd0d9f0893457583982cee5ad44a665c0db5
soriphoono <soriphoono@gmail.com>
2023-10-17 20:19:05 -0500

    added modding tools for gaming

--- .config/yadm/personal/gaming.sh
+++ .config/yadm/personal/gaming.sh
@@ -15,17 +15,21 @@ done
 
 # Install core packages for gaming environment
 packages=(
-    "soundfont-fluid"  # FluidR3 soundfont
-    "boxtron"          # DOSBox fork for Steam Play compatibility
-    "protonup-git"     # Proton updater
-    "protontricks-git" # Proton tricks (configuration tool)
-    "wine"             # Wine
-    "winetricks"       # Wine tricks (configuration tool)
-    "gamemode"         # Game mode
-    "mangohud"         # MangoHud (game overlay)
-    "dxvk-async-git"   # DXVK async (DirectX to Vulkan translation layer)
-    "vkd3d"            # VKD3D (DirectX 12 to Vulkan translation layer)
-    "gamescope-plus"   # Gamescope fork with added patches for stability (for fixing issues in window manager on wayland based compositors) (AUR)
+    "soundfont-fluid"     # FluidR3 soundfont
+    "boxtron"             # DOSBox fork for Steam Play compatibility
+    "steamtinkerlaunch"   # Steam Tinker Launcher
+    "replay-sorcery"      # Replay Sorcery (game replay tool)
+    "protonup-git"        # Proton updater
+    "protontricks-git"    # Proton tricks (configuration tool)
+    "wine"                # Wine
+    "winetricks"          # Wine tricks (configuration tool)
+    "vkbasalt"            # VKBasalt (Vulkan post processing layer)
+    "reshade-shaders-git" # ReShade shaders
+    "gamemode"            # Game mode
+    "mangohud"            # MangoHud (game overlay)
+    "dxvk-async-git"      # DXVK async (DirectX to Vulkan translation layer)
+    "vkd3d"               # VKD3D (DirectX 12 to Vulkan translation layer)
+    "gamescope-plus"      # Gamescope fork with added patches for stability (for fixing issues in window manager on wayland based compositors) (AUR)
 )
 
 # Install proton and it's dependencies
@@ -33,6 +37,7 @@ if $MULTILIB; then
     packages+=(
         "lib32-gamemode" # Game mode (32-bit)
         "lib32-vkd3d"    # VKD3D (32-bit)
+        "lib32-vkbasalt" # VKBasalt (32-bit)
     )
 fi
 
