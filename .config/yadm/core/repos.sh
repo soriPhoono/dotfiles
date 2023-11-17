f7445ee4d518c9dc6deeb97f214197460bdcb785
soriphoono <soriphoono@gmail.com>
2023-11-17 14:54:26 -0600

    Elevated permissions for pacman-key execution

--- .config/yadm/core/repos.sh
+++ .config/yadm/core/repos.sh
@@ -23,10 +23,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 fi
 
 # Setup asus-linux repo
-pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
 curl -o g14.sec "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x8b15a6b0e9a3fa35"
 sudo pacman-key -a g14.sec
 echo -e "[g14]\nServer = https://arch.asus-linux.org" | sudo tee -a /etc/pacman.conf
