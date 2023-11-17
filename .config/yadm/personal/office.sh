6e7fa43fab9b08e3ad84bc3994ae562cd9c0b786
soriphoono <soriphoono@gmail.com>
2023-11-17 15:21:02 -0600

    Removed unneeded packages and formatted

--- .config/yadm/personal/office.sh
+++ .config/yadm/personal/office.sh
@@ -5,14 +5,14 @@
 # Author:       Sori Phoono <soriphoono@gmail.com>
 
 packages=(
-    "thunderbird"
-    "thunderbird-i18n-en-us"
-    "hunspell"
-    "hunspell-en_us"
+	"thunderbird"
+	"thunderbird-i18n-en-us"
+	"hunspell"
+	"hunspell-en_us"
 )
 
 echo "Installing office tools..."
-paru -S --noconfirm --needed ${packages[@]}
+paru -S --noconfirm --needed "${packages[@]}"
 echo "Done!"
 
 paru -c --noconfirm >/dev/null
