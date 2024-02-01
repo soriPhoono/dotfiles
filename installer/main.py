0fc6c3705bdbf709dd52211125ad4aeaa55df0bc
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-01-31 22:25:09 -0600

    Moved installer
    Moved installer file for nix-shell

--- installer/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-def main():
-    '''Main function of installer'''
-
-    
---
+++ installer/src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import asyncio
+
+
+def main():
+    '''Main function of installer'''
+
+    
+
+    pass
+
+    
+if __name__ == '__main__':
+    main()
