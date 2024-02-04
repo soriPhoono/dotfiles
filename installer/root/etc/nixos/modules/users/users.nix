1945ec757abf42088cefea95b85b67075a5bc33d
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 12:29:34 -0600

    Add users.nix module for home-manager configuration

---
+++ installer/root/etc/nixos/modules/users/users.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  imports = [
+    <home-manager/nixos>
+
+    # Import user configurations for various users on system
+    ./soriphoono.nix
+  ]
+
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+}
