e2ccdb8817eef9d18107852f5427446b7d566fe9
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 14:20:17 -0600

    Set hostname to 'home_desktop' and enable NetworkManager

--- installer/root/etc/nixos/modules/cli.nix
+++ installer/root/etc/nixos/modules/cli.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }: {
   networking = {
+    hostName = "home_desktop"; # Set the hostname to ‘nixos’
+
     networkmanager.enable = true; # Enable NetworkManager to manage network connections
   };
 
