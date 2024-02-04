2e14e081ebd4e5d61e5ece9ad3d12fbc63282442
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 15:00:45 -0600

    Added overlay for discord updating

---
+++ installer/root/etc/nixos/modules/personal/general.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+      discord = super.discord.overrideAttrs (
+        _: { src = builtins.fetchTarball {
+          url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+        }}
+      )
+    })
+  ];
+}
