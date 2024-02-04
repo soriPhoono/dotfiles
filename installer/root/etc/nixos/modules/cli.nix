7c4ce3938f74c2ec85ee0103f861357f0f622214
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 12:31:46 -0600

    Enable git and configure default branch and URL prefixes

--- installer/root/etc/nixos/modules/cli.nix
+++ installer/root/etc/nixos/modules/cli.nix
@@ -30,21 +30,19 @@
       };
     };
 
-    programs = {
-      git = {
-        enable = true; # Enable git
+    git = {
+      enable = true; # Enable git
 
-        config = {
-          init = {
-            defaultBranch = "main"; # Use ‘main’ as the default branch
-          };
-          url = {
-            "https://github.com/" = {
-              insteadOf = [
-                "gh:" # Use ‘gh:’ as a prefix for GitHub URLs
-                "github:" # Use ‘github:’ as a prefix for GitHub URLs
-              ];
-            };
+      config = {
+        init = {
+          defaultBranch = "main"; # Use ‘main’ as the default branch
+        };
+        url = {
+          "https://github.com/" = {
+            insteadOf = [
+              "gh:" # Use ‘gh:’ as a prefix for GitHub URLs
+              "github:" # Use ‘github:’ as a prefix for GitHub URLs
+            ];
           };
         };
       };
