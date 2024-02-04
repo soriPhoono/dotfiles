41c6eff26dc9afa707a8f8c29483bf5805dad79b
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-03 23:01:40 -0600

    Update shell.nix
    formatting

--- installer/shell.nix
+++ installer/shell.nix
@@ -1,9 +1,9 @@
 let
-    pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
-    packages = [
-        (pkgs.python3.withPackages (python-pkgs: [
-            python-pkgs.pyyaml
-        ]))
-    ];
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.pyyaml
+    ]))
+  ];
 }
