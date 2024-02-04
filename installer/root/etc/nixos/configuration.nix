8d174a7ef65574e16fbcb1b0c5e8158392096da6
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-03 23:03:37 -0600

    Edited *.nix
    Began writing basic system installation package, going for an iterative approach

--- installer/root/etc/nixos/configuration.nix
+++ installer/root/etc/nixos/configuration.nix
@@ -1,19 +1,79 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
 
+    # Import the rest of the configuration.
     ./modules/boot.nix
   ];
 
-  programs = {
-    hyprland.enable = true;
+  # Use the systemd-boot bootloader.
+  boot = {
+    loader = {
+      timeout = 3;
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max";
+      };
+    };
+
+    plymouth = {
+      enable = true;
+      themePackages = [ pkgs.catppuccin-plymouth ];
+      theme = "catppuccin-mocha";
+    };
+
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "50%"; # 50% of RAM, increase this if nix builds start to fail due to lack of space
+      cleanOnBoot = true;
+    };
   };
 
-  services.pipe
+  zramSwap.enable = true;
 
-  xdg.portal.wlr.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
 
+      autosuggestions = {
+        enable = true;
+        strategy = [
+          "history"
+          "completion"
+          "match_prev_cmd"
+        ];
+      };
+
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [
+          "main"
+          "brackets"
+          "pattern"
+          "cursor"
+          "regexp"
+          "root"
+          "line"
+        ];
+      };
+    };
+
+    git = {
+      enable = true;
+
+      config = {
+        userName = "soriphoono";
+        userEmail = "soriphoono@gmail.com";
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    pkgs.ntfs3g
+  ];
+
+  # Enable automatic updates and set the reboot window.
   system = {
     autoUpgrade = {
       enable = true;
