0948a692931cea52ce4ca6e88674ec3807a80057
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 12:29:23 -0600

    Remove unused code and update Git configuration

--- installer/root/etc/nixos/modules/users/soriphoono.nix
+++ installer/root/etc/nixos/modules/users/soriphoono.nix
@@ -15,36 +15,4 @@
       "networkmanager" # Enable ‘networkmanager’ for the user
     ];
   };
-
-  home-manager.users.soriphoono = { pkgs, ... }: {
-    home.packages = [
-
-    ];
-
-    programs = {
-      git = {
-        enable = true; # Enable git
-
-        config = {
-          user = {
-            name = "soriphoono"; # Git user name
-            email = "soriphoono@gmail.com"; # Git user email
-          };
-          init = {
-            defaultBranch = "main"; # Use ‘main’ as the default branch
-          };
-          url = {
-            "https://github.com/" = {
-              insteadOf = [
-                "gh:" # Use ‘gh:’ as a prefix for GitHub URLs
-                "github:" # Use ‘github:’ as a prefix for GitHub URLs
-              ];
-            };
-          };
-        };
-      };
-    };
-
-    home.stateVersion = "23.11"; # Version of the state format
-  };
 }
