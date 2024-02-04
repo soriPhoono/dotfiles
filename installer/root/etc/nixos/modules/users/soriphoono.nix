532a02b971331fa721c6332974077817191b4227
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-04 14:51:08 -0600

    Update user configuration in NixOS module

--- installer/root/etc/nixos/modules/users/soriphoono.nix
+++ installer/root/etc/nixos/modules/users/soriphoono.nix
@@ -4,7 +4,6 @@
   ];
 
   users.users.soriphoono = {
-    name = "soriphoono"; # User name
     description = "Sori Phoono"; # Full name
     password = "password"; # Initial password
 
@@ -12,14 +11,17 @@
 
     extraGroups = [
       "wheel" # Enable ‘sudo’ for the user
+      "video" # Enable video acceleration for the user
+      "audio" # Enable audio for the user
       "networkmanager" # Enable ‘networkmanager’ for the user
     ];
   };
 
   home-manager.users.soriphoono = { pkgs, ... }: {
-    home.packages = [
-      pkgs.btop
-    ]; # List packages installed in the user environment
+    # List packages installed in the user environment
+    home.packages = with pkgs; [
+      btop
+    ];
     # programs.zsh.enable = true; # Enable zsh
 
     # Version of the state format should match the version of Home Manager.
