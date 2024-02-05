d36a1a3a5d1a43de42338431af01430fe9ee0298
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-05 16:24:50 -0600

    added basic boot instructions

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -3,6 +3,18 @@
     ./hardware-configuration.nix  # Include the results of the hardware scan.
   ];
 
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_7; # Use the 6.7 kernel.
+
+    tmp = {
+      useTmpfs = true; # Use a tmpfs for /tmp.
+      tmpfsSize = "50%"; # 50% of RAM, increase this if nix builds start to fail due to lack of space.
+      cleanOnBoot = true; # Clean /tmp on boot.
+    };
+  };
+
+  zramSwap.enable = true; # Enable zram swap.
+
   time.timeZone = "America/Chicago"; # Set the time zone to America/Chicago
   i18n.defaultLocale = "en_US.UTF-8"; # Set the default locale to en_US.UTF-8
 
