382984b83961e7ec3db399bf05230e029f8828f0
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-05 16:25:40 -0600

    added systemd-boot

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -6,6 +6,18 @@
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_6_7; # Use the 6.7 kernel.
 
+    # Use the systemd-boot bootloader.
+    loader = {
+      timeout = 3; # Reduce the timeout to 3 seconds.
+
+      efi.canTouchEfiVariables = true; # Allow the bootloader to modify EFI variables.
+
+      systemd-boot = {
+        enable = true; # This is the default, but let's be explicit.
+        consoleMode = "max"; # Enable more detailed output.
+      };
+    };
+
     tmp = {
       useTmpfs = true; # Use a tmpfs for /tmp.
       tmpfsSize = "50%"; # 50% of RAM, increase this if nix builds start to fail due to lack of space.
