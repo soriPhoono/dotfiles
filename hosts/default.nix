64f411b136ec58f16a8cdaed713c42466a8f52f0
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-10 17:02:51 -0600

    Add extraSpecialArgs and imports to home-manager configuration

--- hosts/default.nix
+++ hosts/default.nix
@@ -20,9 +20,13 @@ in {
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit pkgs username;
+        };
 
-        home-manager.users."${username}" =
-          (import ../modules/home.nix { inherit pkgs username; }); # Import the home.nix file
+        home-manager.users."${username}" = {
+          imports = [(import ../modules/home.nix)] ++ [(import ./vm/home.nix)];
+        };
       }
     ];
   };
