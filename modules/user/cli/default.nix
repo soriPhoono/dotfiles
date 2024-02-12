32a10907c1794545b3cfe160dce01b5a64653549
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-12 01:58:05 -0600

    Update git configuration in user CLI module

--- modules/user/cli/default.nix
+++ modules/user/cli/default.nix
@@ -17,6 +17,44 @@
   };
 
   programs = {
+    git = {
+      enable = true;
+
+      userName = "${username}";
+      userEmail = "${username}@gmail.com";
+
+      delta = {
+        enable = true;
+
+        options = {
+          features = "decorations";
+
+          true-color = "always";
+          whitespace-error-style = "22 reverse";
+
+          line-numbers = true;
+
+          side-by-side = true;
+          line-numbers-left-format = "";
+          line-numbers-right-format = "| ";
+
+          hyperlinks = true;
+          hyperlinks-file-link-format = "vscode://file/{path}:{line}"; # TODO: possible bad code
+
+          merge = {
+            conflictstyle = "diff3";
+          };
+
+          decorations = {
+            commit-decoration-style = "bold yellow box ul";
+            file-style = "bold yellow ul";
+            file-decoration-style = "none";
+            hunk-header-decoration-style = "yellow box";
+          };
+        };
+      };
+    };
+
     eza = {
       enable = true;
 
