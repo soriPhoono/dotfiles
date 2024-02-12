36244ecc2e5c82f7ac9643e1f91d04357654ead0
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-12 13:39:11 -0600

    Update hyprland.nix with environment and monitor configurations

--- modules/user/desktop/hyprland.nix
+++ modules/user/desktop/hyprland.nix
@@ -3,7 +3,30 @@
     enable = true;
 
     settings = {
-      
+      settings = {
+        # Environment variables
+        env = {
+          "XCURSOR_SIZE" = 24;
+        };
+
+        # Monitor configurations
+
+        # General configuration options
+
+        # Decoration configuration options
+
+        # Animation configuration options
+
+        # Layout configuration options (master, dwindle)
+
+        # Input configuration options
+
+        # Miscellaneous configuration options
+
+        # Keybinding configuration options
+
+        # Window rules and opacity configuration options
+      };
     };
   };
 }
