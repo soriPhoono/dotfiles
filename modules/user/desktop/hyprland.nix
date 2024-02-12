feaba101efc9edc762c171af57ebd9d7ff35c6ff
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-12 13:54:34 -0600

    fixed unneeded double nest

--- modules/user/desktop/hyprland.nix
+++ modules/user/desktop/hyprland.nix
@@ -3,30 +3,28 @@
     enable = true;
 
     settings = {
-      settings = {
-        # Environment variables
-        env = {
-          "XCURSOR_SIZE" = 24;
-        };
-
-        # Monitor configurations
-
-        # General configuration options
-
-        # Decoration configuration options
-
-        # Animation configuration options
-
-        # Layout configuration options (master, dwindle)
-
-        # Input configuration options
-
-        # Miscellaneous configuration options
-
-        # Keybinding configuration options
-
-        # Window rules and opacity configuration options
+      # Environment variables
+      env = {
+        "XCURSOR_SIZE" = 24;
       };
+
+      # Monitor configurations
+
+      # General configuration options
+
+      # Decoration configuration options
+
+      # Animation configuration options
+
+      # Layout configuration options (master, dwindle)
+
+      # Input configuration options
+
+      # Miscellaneous configuration options
+
+      # Keybinding configuration options
+
+      # Window rules and opacity configuration options
     };
   };
 }
