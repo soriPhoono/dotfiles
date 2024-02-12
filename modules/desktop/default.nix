89bb7dccee175eaeb6e538aefc9643b3d3b69eb7
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-12 00:01:37 -0600

    Update desktop.nix to include xdg-desktop-portal-hyprland package

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,8 +3,8 @@
     portal = {
       enable = true;
       xdgOpenUsePortal = true;
-      configPackages = pkgs.xdg-desktop-portal-hyprland;
-      extraPortals = [
+      configPackages = [
+        pkgs.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
       ];
     };
