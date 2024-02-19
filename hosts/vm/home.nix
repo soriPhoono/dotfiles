ed1b325295710eb7eafd9c0f9c8dd216473cd87e
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-19 13:33:27 -0600

    fixed hyprland home config routing

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -1,8 +1,5 @@
 { ... }: {
   imports = [
-    ../../modules/user/desktop/hyprland.nix
+    ../../home/desktop/hyprland.nix
   ];
-
-  # xdg.configFile."hypr/modules/monitors.conf".source =
-  #   ../../config/hypr/modules/laptop.conf
 }
