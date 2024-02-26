eb83fa3afc2f37372e4e9040da513edeb022000c
Sori Phoono <96075107+soriPhoono@users.noreply.github.com>
2024-02-25 21:32:22 -0600

    set login screen background

--- system/desktops/hyprland.nix
+++ system/desktops/hyprland.nix
@@ -3,17 +3,6 @@
     ./regreet.nix
   ];
 
-  services = {
-    greetd = {
-      settings = {
-        default_session = {
-          # TODO: change the wayland compositor to hyprland
-          command = "${pkgs.cage}/bin/cage -c -- ${pkgs.greetd.regreet}/bin/regreet";
-        };
-      };
-    };
-  };
-
   programs.hyprland = {
     enable = true;
     enableNvidiaPatches = true;
