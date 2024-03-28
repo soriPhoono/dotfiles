ffbfa72e74124adb22be06537d7a4baebb911b70
soriphoono <soriphoono@gmail.com>
2024-03-27 23:45:39 -0500

    Updated variable naming, and added installation confirmation method

--- installer/util.sh
+++ installer/util.sh
@@ -1,13 +1,29 @@
 function inform_user {
-  prompt=$1
+  p=$1
 
   echo -e "\n> $1"
 }
 
 function debug_print {
-  prompt=$1
+  p=$1
 
   if [[ $(whoami) == "soriphoono" ]]; then
-    inform_user $prompt
+    inform_user $p
+  fi
+}
+
+function confirm_installation {
+  p=$1
+  callback=$2
+
+  echo "$p"
+  read -n 1 -p "(Y/n) >> " response
+
+  if [[ $(echo $response | tr '[:upper:]' '[:lower:]') == "y" ]]; then
+    echo -e "\nInstallation confirmed. Proceeding...\n"
+
+    command $callback
+  else
+    echo -e "\nInstallation cancelled. Skipping...\n"
   fi
 }
