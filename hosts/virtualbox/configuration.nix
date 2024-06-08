c637c9ad59d7340a65142558d140d4aae49e71b5
soriphoono <soriphoono@gmail.com>
2024-06-08 22:43:14 +0000

    chore: Add KDE desktop configuration

--- hosts/virtualbox/configuration.nix
+++ hosts/virtualbox/configuration.nix
@@ -6,6 +6,8 @@
     ../../modules/nixos/programs/gpg.nix
     ../../modules/nixos/programs/gamemode.nix
     ../../modules/nixos/programs/steam.nix
+
+    ../../modules/nixos/desktops/kde.nix
   ];
 
   system.stateVersion = "${vars.stateVersion}";
