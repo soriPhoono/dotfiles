53d27c97d5016988a1a4cc71f2708971a6c4bfec
soriphoono <soriphoono@gmail.com>
2024-06-08 22:42:08 +0000

    Added hyprland configs

--- modules/home-manager/desktops/hyprland.nix
+++ modules/home-manager/desktops/hyprland.nix
@@ -31,10 +31,6 @@
     enable = true;
 
     configDir = ../../../ags;
-
-    extraPackages = with pkgs; [
-
-    ];
   };
 
   wayland.windowManager.hyprland = {
@@ -46,6 +42,54 @@
       monitor = [
         ",1920x1080@144,0x0,1.5"
       ];
+
+      input = {
+        kb_layout = "us";
+        touchpad = {
+          natural_scroll = "yes";
+          disable_while_typing = "yes";
+        };
+        sensitivity = 0.1;
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 3;
+        layout = "dwindle";
+        allow_tearing = false;
+      };
+
+      decoration = {
+        rounding = 7;
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_range_power = 3;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+      };
+
+      animations = {
+        enabled = 1;
+        bezier = "overshot,0.13,0.99,0.29,1.1,";
+        animation = [
+          "fade,1,4,default"
+          "workspaces,1,4,default,fade"
+          "windows,1,4,overshot,popin 95%"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
+
+      master.new_is_master = true;
+      gestures.workspace_swipe = "on";
+      misc.force_default_wallpaper = 1;
 
       exec-once = [
         "wl-paste --type text --watch cliphist store"
@@ -65,7 +109,22 @@
 
         "$mod, Return, exec, alacritty"
         "$mod, B, exec, firefox"
-      ];
+      ] ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mainMod SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
+            ]
+          )
+            10)
+        );
 
       binde = [
         ", XF86AudioRaiseVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%"
