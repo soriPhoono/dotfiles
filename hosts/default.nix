324956020c46b33c07fb69f2330d63cf3bae7fc7
soriphoono <soriphoono@gmail.com>
2024-06-08 02:04:25 +0000

    Updated for framework build target

--- hosts/default.nix
+++ hosts/default.nix
@@ -15,4 +15,21 @@
   in (import ./wsl {
     inherit inputs pkgs vars;
   });
+
+  framework = let
+    vars = {
+      system = "x86_64-linux";
+      stateVersion = "24.05";
+
+      defaultUser = "soriphoono";
+    };
+
+    pkgs = import inputs.nixpkgs {
+      inherit (vars) system;
+
+      config.allowUnfree = true;
+    };
+  in (import ./framework {
+    inherit inputs pkgs vars;
+  });
 }
