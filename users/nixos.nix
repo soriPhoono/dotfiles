11dc5fda82805ca9bd39b9363e09d719a6b85e02
soriphoono <soriphoono@gmail.com>
2024-06-08 07:41:44 +0000

    Began writing themes

--- users/nixos.nix
+++ users/nixos.nix
@@ -2,12 +2,24 @@
   imports = [
     ./modules/core/xdg-user-dirs.nix
 
-    ./modules/programs
+    ./modules/programs/bat.nix
+    ./modules/programs/eza.nix
+    ./modules/programs/fastfetch.nix
+    ./modules/programs/fish.nix
+    ./modules/programs/git.nix
+    ./modules/programs/nvim.nix
+    ./modules/programs/starship.nix
   ];
 
   home.packages = with pkgs; [
     nil
     nixpkgs-fmt
+
+    cargo
+    clippy
+    rustc
+    rustfmt
+    rust-analyzer
   ];
 
   programs.home-manager.enable = true;
