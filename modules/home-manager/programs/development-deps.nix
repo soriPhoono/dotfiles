f96fd83f7ef5378e4d19eb8a02c5a60e35fec9d1
soriphoono <soriphoono@gmail.com>
2024-06-08 15:22:03 +0000

    feat: Add mkdocs and update development dependencies in home-manager programs

--- modules/home-manager/programs/development-deps.nix
+++ modules/home-manager/programs/development-deps.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     # Core development packages
-    gitkraken
+    mkdocs
 
     # Nix development deps
     nil
@@ -11,14 +11,35 @@
     nodePackages.bash-language-server
     shfmt
 
+    # Haskell
+    haskell.package-list
+
     # C/C++ development deps
-    
+    clang-tools
+    lldb
+    meson
+
+    # Zig development deps
+    zig
+    zls
 
     # Rust development deps
-    cargo
-    clippy
     rustc
+    clippy
     rustfmt
+    cargo
     rust-analyzer
+
+    # OpenJDK development deps
+    jdk
+    jdt-language-server
+
+    # Python development deps
+    python3
+
+    # Web/JavaScript
+    sass
+    bun
+    nodePackages.typescript-language-server
   ];
 }
