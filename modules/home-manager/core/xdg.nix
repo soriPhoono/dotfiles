2f659c59b3c294e3edff5a273130b4f571c7c9e7
soriphoono <soriphoono@gmail.com>
2024-06-08 09:39:24 +0000

    feat: Add Google productivity apps to XDG desktop entries

--- modules/home-manager/core/xdg.nix
+++ modules/home-manager/core/xdg.nix
@@ -7,6 +7,50 @@
     enable = true;
 
     desktopEntries = {
+      google-calendar = {
+        name = "Google Calendar";
+        genericName = "Calendar";
+
+        exec = "firefox https://calendar.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
+        ];
+        mimeType = [
+          "text/calendar"
+        ];
+      };
+      google-drive = {
+        name = "Google Drive";
+        genericName = "Cloud storage";
+
+        exec = "firefox https://drive.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
+        ];
+        mimeType = [
+          "text/pdf"
+          "application/vnd.google-apps.file"
+        ];
+      };
+      gmail = {
+        name = "Gmail";
+        genericName = "Email client";
+
+        exec = "firefox https://mail.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Network"
+          "Email"
+        ];
+        mimeType = [
+          "message/rfc822"
+        ];
+      };
       google-docs = {
         name = "Google Docs";
         genericName = "Document editor";
@@ -20,6 +64,58 @@
         mimeType = [
           "text/pdf"
           "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ];
+      };
+      google-slides = {
+        name = "Google Slides";
+        genericName = "Presentation editor";
+
+        exec = "firefox https://slides.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
+        ];
+        mimeType = [
+          "text/pdf"
+          "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        ];
+      };
+      google-sheets = {
+        name = "Google Sheets";
+        genericName = "Spreadsheet editor";
+
+        exec = "firefox https://sheets.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
+        ];
+        mimeType = [
+          "text/pdf"
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        ];
+      };
+      google-tasks = {
+        name = "Google Tasks";
+        genericName = "Task manager";
+
+        exec = "firefox https://tasks.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
+        ];
+      };
+      google-keep = {
+        name = "Google Keep";
+        genericName = "Note-taking app";
+
+        exec = "firefox https://keep.google.com/";
+        terminal = false;
+        categories = [
+          "Application"
+          "Office"
         ];
       };
     };
