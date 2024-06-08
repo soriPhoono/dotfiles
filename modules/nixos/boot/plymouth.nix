d12fbc25effd33086e6a8413e6ebc67f610d3f43
soriphoono <soriphoono@gmail.com>
2024-06-08 18:58:29 +0000

    feat: Update plymouth theme to "rings"

--- modules/nixos/boot/plymouth.nix
+++ modules/nixos/boot/plymouth.nix
@@ -1,3 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   boot.plymouth.enable = true;
+
+  boot.plymouth = {
+    themePackages = with pkgs; [
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [
+          "rings"
+        ];
+      })
+    ];
+
+    theme = "rings";
+  };
 }
