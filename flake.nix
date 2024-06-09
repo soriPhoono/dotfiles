53a41ea4bdf73ba2537c932c9a5993e398ca2390
soriphoono <soriphoono@gmail.com>
2024-06-09 22:27:19 +0000

    Rolled back to 24.05

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Personal computer configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -26,12 +26,6 @@
         nixpkgs.follows = "nixpkgs";
         home-manager.follows = "home-manager";
       };
-    };
-
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
     };
 
     ags.url = "github:Aylur/ags";
