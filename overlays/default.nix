55c13f7c6ff46fc1aeb9bccef8475e6ffa70c719
soriphoono <soriphoono@gmail.com>
2024-06-10 17:18:27 +0000

    chore: Remove commented out code in default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,4 @@
 [
-/*
-  (final: prev: {
-    vscode = prev.vscode.override {
-      _ = {
-        src = fetchTarball "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64";
-      };
-    };
-  })
-*/
   (final: prev: {
     discord = prev.discord.override {
       _ = {
