780aa01f34836633351730c2b6c7dd15eb81536e
soriphoono <soriphoono@gmail.com>
2024-06-10 15:31:14 +0000

    chore: Removed remote play for default impl, not applicable on laptop, remove proton-ge

--- modules/nixos/programs/steam.nix
+++ modules/nixos/programs/steam.nix
@@ -1,16 +1,11 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs = {
     steam = {
       enable = true;
 
       extest.enable = true;
-      remotePlay.openFirewall = true;
 
       protontricks.enable = true;
-
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
     };
   };
 }
