a9ff2953a7d3bec0e775926c0166272102913852
soriphoono <soriphoono@gmail.com>
2024-06-10 14:23:50 +0000

    feat: Update vscode source URL for lazy loading images

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,12 @@
 [
   (final: prev: {
+    vscode = prev.vscode.override {
+      _ = {
+        src = fetchTarball "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64";
+      };
+    };
+  })
+  (final: prev: {
     discord = prev.discord.override {
       _ = {
         src = fetchTarball "https://discord.com/api/download?platform=linux&format=tar.gz";
