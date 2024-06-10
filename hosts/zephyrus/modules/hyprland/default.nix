61ad877a3ab0010f0d79d132c5b44a62684cc997
soriphoono <soriphoono@gmail.com>
2024-06-10 23:09:57 +0000

    added laptop specific configs

--- hosts/zephyrus/modules/hyprland/default.nix
+++ hosts/zephyrus/modules/hyprland/default.nix
@@ -1,3 +1,19 @@
 { ... }: {
-  
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1,1920x1080@144,0x0,1,vrr,2"
+    ];
+
+    input = {
+      kb_layout = "us";
+      repeat_delay = 400;
+      sensitivity = 0.1;
+      accel_profile = "flat";
+      touchpad = {
+        natural_scroll = true;
+        disable_while_typing = true;
+        clickfinger_behavior = true;
+      };
+    };
+  };
 }
