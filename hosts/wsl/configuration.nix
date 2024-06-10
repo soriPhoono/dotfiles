c6a1ace735a51cd8b06c73342293cbdbbaaf8c9d
soriphoono <soriphoono@gmail.com>
2024-06-10 12:53:51 +0000

    Added nixos development tools

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -3,6 +3,11 @@
     ../../modules/nixos/core/core.nix
   ];
 
+  environment.systemPackages = with pkgs; [
+    nil
+    nixpkgs-fmt
+  ];
+
   programs.nix-ld = {
     enable = true;
     package = pkgs.nix-ld-rs;
