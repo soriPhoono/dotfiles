ef5f6f86a9d9f66d4da95a162846f50b63fe1960
soriphoono <soriphoono@gmail.com>
2024-06-10 14:22:19 +0000

    feat: Add yt-dlp, Hyprland theme configuration, and other updates to home-manager module

--- users/soriphoono.nix
+++ users/soriphoono.nix
@@ -2,10 +2,22 @@
   imports = [
     ../modules/home-manager/core
 
-    ../modules/home-manager/programs
-    ../modules/home-manager/services
+    ../modules/home-manager/programs/alacritty.nix
+    ../modules/home-manager/programs/bat.nix
+    ../modules/home-manager/programs/development-deps.nix
+    ../modules/home-manager/programs/eza.nix
+    ../modules/home-manager/programs/fastfetch.nix
+    ../modules/home-manager/programs/fish.nix
+    ../modules/home-manager/programs/git.nix
+    ../modules/home-manager/programs/nvim.nix
+    ../modules/home-manager/programs/prism-launcher.nix
+    ../modules/home-manager/programs/starship.nix
+    ../modules/home-manager/programs/vscode.nix
+    ../modules/home-manager/programs/yt-dlp.nix
 
-    ../modules/home-manager/desktops/hyprland.nix
+    ../modules/home-manager/services/mpris-proxy.nix
+
+    ../modules/home-manager/hyprland
 
     ../modules/home-manager/themes/catppuccin.nix
   ];
