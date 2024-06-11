e0c2dfd98b048f59b0419b1db25aa977c82ce1af
soriphoono <soriphoono@gmail.com>
2024-06-11 12:10:54 +0000

    configured mpd

---
+++ modules/home-manager/hyprland/mpd.nix
@@ -0,0 +1,24 @@
+{ ... }: {
+  services = {
+    mpd = {
+      enable = true;
+
+      network.startWhenNeeded = true;
+    };
+
+    mpd-mpris = {
+      enable = true;
+      mpd.useLocal = true;
+    };
+
+    mpd-discord-rpc = {
+      enable = true;
+
+      hosts = [ "localhost:6600" ];
+      format = {
+        details = "$title";
+        state = "On $album by $artist";
+      };
+    };
+  };
+}
