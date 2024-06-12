b6c733ae20391ed08995245d082028fd65390b76
soriphoono <soriphoono@gmail.com>
2024-06-12 17:10:18 +0000

    fixed paths

--- nixos/hosts/zephyrus/modules/kde/default.nix
+++ nixos/hosts/zephyrus/modules/kde/default.nix
@@ -1,5 +1,18 @@
 { inputs, ... }: {
   imports = [
     inputs.plasma-manager.homeManagerModules.plasma-manager
+
+    ../../../../modules/home-manager/programs/bat.nix
+    ../../../../modules/home-manager/programs/development-deps.nix
+    ../../../../modules/home-manager/programs/eza.nix
+    ../../../../modules/home-manager/programs/fastfetch.nix
+    ../../../../modules/home-manager/programs/fish.nix
+    ../../../../modules/home-manager/programs/gaming.nix
+    ../../../../modules/home-manager/programs/git.nix
+    ../../../../modules/home-manager/programs/nvim.nix
+    ../../../../modules/home-manager/programs/vscode.nix
+    ../../../../modules/home-manager/programs/yt-dlp.nix
+
+    ../../../../modules/home-manager/services/mpris-proxy.nix
   ];
 }
--- nixos/users/soriphoono.nix
+++ nixos/users/soriphoono.nix
@@ -3,19 +3,6 @@
     ../modules/home-manager/core/xdg.nix
     ../modules/home-manager/core/utility-progs.nix
 
-    ../modules/home-manager/programs/bat.nix
-    ../modules/home-manager/programs/development-deps.nix
-    ../modules/home-manager/programs/eza.nix
-    ../modules/home-manager/programs/fastfetch.nix
-    ../modules/home-manager/programs/fish.nix
-    ../modules/home-manager/programs/gaming.nix
-    ../modules/home-manager/programs/git.nix
-    ../modules/home-manager/programs/nvim.nix
-    ../modules/home-manager/programs/vscode.nix
-    ../modules/home-manager/programs/yt-dlp.nix
-
-    ../modules/home-manager/services/mpris-proxy.nix
-
     ../modules/home-manager/themes/catppuccin.nix
   ];
 
