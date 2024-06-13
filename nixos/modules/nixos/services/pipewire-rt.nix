54e5c13ed4555ea32a2f939d2c26da4c8b239c2b
soriphoono <soriphoono@gmail.com>
2024-06-13 14:47:47 -0500

    created special pipewire-rt config

---
+++ nixos/modules/nixos/services/pipewire-rt.nix
@@ -0,0 +1,55 @@
+{ pkgs, vars, ... }: {
+  security.rtkit.enable = true;
+
+  services = {
+    pipewire = {
+      enable = true;
+
+      pulse.enable = true;
+
+      alsa.enable = true;
+      alsa.support32Bit = true;
+
+      jack.enable = true;
+
+      wireplumber.configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
+          alsa_monitor.rules = {
+            {
+              matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+              apply_properties = {
+                ["audio.format"] = "S32LE",
+                ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+                ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+                -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+              },
+            },
+          }
+        '')
+      ];
+    };
+
+    pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
+      };
+    };
+  };
+
+  users.users.${vars.defaultUser}.extraGroups = [
+    "audio"
+  ];
+}
--- nixos/modules/nixos/services/pipewire.nix
+++ nixos/modules/nixos/services/pipewire.nix
@@ -1,4 +1,4 @@
-{ pkgs, vars, ... }: {
+{ vars, ... }: {
   security.rtkit.enable = true;
 
   services = {
@@ -11,41 +11,6 @@
       alsa.support32Bit = true;
 
       jack.enable = true;
-
-      wireplumber.configPackages = [
-        (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
-          alsa_monitor.rules = {
-            {
-              matches = {{{ "node.name", "matches", "alsa_output.*" }}};
-              apply_properties = {
-                ["audio.format"] = "S32LE",
-                ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
-                ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
-                -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
-              },
-            },
-          }
-        '')
-      ];
-    };
-
-    pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
-      context.modules = [
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            pulse.min.req = "32/48000";
-            pulse.default.req = "32/48000";
-            pulse.max.req = "32/48000";
-            pulse.min.quantum = "32/48000";
-            pulse.max.quantum = "32/48000";
-          };
-        }
-      ];
-      stream.properties = {
-        node.latency = "32/48000";
-        resample.quality = 1;
-      };
     };
   };
 
