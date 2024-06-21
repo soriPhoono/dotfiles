c45f59058c2631a27a00a7a542dda0d740db6114
soriphoono <soriphoono@gmail.com>
2024-06-21 09:54:07 -0500

    Updated format for gpu packages

--- nixos/hosts/home-desktop/configuration.nix
+++ nixos/hosts/home-desktop/configuration.nix
@@ -43,9 +43,22 @@
     ];
   };
 
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD";
-    VDPAU_DRIVER = "va_gl";
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
+  environment = {
+    sessionVariables = {
+      KWIN_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card2";
+
+      LIBVA_DRIVER_NAME = "iHD";
+      VDPAU_DRIVER = "va_gl";
+    };
+
+    systemPackages = with pkgs; [
+      intel-gpu-tools
+      radeontop
+    ];
   };
 
   system.stateVersion = "${stateVersion}";
