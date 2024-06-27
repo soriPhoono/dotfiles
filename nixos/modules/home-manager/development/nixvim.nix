3d4cc482f0af2e827b17443a5fb087671639830c
soriphoono <soriphoono@gmail.com>
2024-06-27 13:23:04 -0500

    Updated neovim configuration

--- nixos/modules/home-manager/development/nixvim.nix
+++ nixos/modules/home-manager/development/nixvim.nix
@@ -19,6 +19,26 @@
 
     editorconfig.enable = true;
 
+    plugins = {
+      neo-tree = {
+        enable = true;
 
+        popupBorderStyle = "rounded";
+      };
+      telescope = {
+        enable = true;
+
+        extensions = {
+          file-browser.enable = true;
+          fzf-native.enable = true;
+        };
+      };
+      treesitter.enable = true;
+      lsp.enable = true;
+      cmp.enable = true;
+      cmp-buffer.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-treesitter.enable = true;
+    };
   };
 }
