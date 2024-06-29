b5482deefb2fac01d963f2422b7c976c1c55162c
soriphoono <soriphoono@gmail.com>
2024-06-28 19:30:11 -0500

    Updated development suite layout

--- nixos/hosts/home-desktop/modules/kde.nix
+++ nixos/hosts/home-desktop/modules/kde.nix
@@ -3,7 +3,6 @@
     inputs.plasma-manager.homeManagerModules.plasma-manager
 
     ../../../modules/home-manager/core
-    ../../../modules/home-manager/development
     ../../../modules/home-manager/programs
 
     ../../../modules/home-manager/services/mpris-proxy.nix
--- nixos/modules/home-manager/programs/default.nix
+++ nixos/modules/home-manager/programs/default.nix
@@ -1,6 +1,7 @@
 { ... }: {
   imports = [
     ./cli.nix
+    ./development.nix
     ./gaming.nix
     ./streaming.nix
     ./utilities.nix
