e3d0171b66f4955fd4f2e9e8293cce50018aab68
soriphoono <soriphoono@gmail.com>
2024-06-29 05:49:32 -0500

    Formatting

--- nixos/modules/home-manager/nvim/plugins/lsp.nix
+++ nixos/modules/home-manager/nvim/plugins/lsp.nix
@@ -1,99 +1,99 @@
 { pkgs, ... }: {
   programs.nixvim.plugins = {
-      lspkind = {
-        enable = true;
-
-        cmp = {
-          enable = true;
-
-          menu = {
-            nvim_lsp = "[LSP]";
-            nvim_lua = "[api]";
-            path = "[path]";
-            luasnip = "[snip]";
-            buffer = "[buffer]";
-            neorg = "[neorg]";
-            cmp_tabby = "[Tabby]";
-          };
-        };
-      };
-
-      luasnip.enable = true;
+    lspkind = {
+      enable = true;
 
       cmp = {
         enable = true;
 
-        settings = {
-          snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-
-          mapping = {
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = "cmp.mapping.close()";
-            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-          };
-
-          sources = [
-            {name = "path";}
-            {name = "nvim_lsp";}
-            {name = "cmp_tabby";}
-            {name = "luasnip";}
-            {
-              name = "buffer";
-              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-            }
-            {name = "neorg";}
-          ];
+        menu = {
+          nvim_lsp = "[LSP]";
+          nvim_lua = "[api]";
+          path = "[path]";
+          luasnip = "[snip]";
+          buffer = "[buffer]";
+          neorg = "[neorg]";
+          cmp_tabby = "[Tabby]";
         };
       };
+    };
+
+    luasnip.enable = true;
+
+    cmp = {
+      enable = true;
+
+      settings = {
+        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+          { name = "cmp_tabby"; }
+          { name = "luasnip"; }
+          {
+            name = "buffer";
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          { name = "neorg"; }
+        ];
+      };
+    };
     lsp = {
-        enable = true;
+      enable = true;
 
-        keymaps = {
-          silent = true;
+      keymaps = {
+        silent = true;
 
-          diagnostic = {
-            "<C-Left>" = "goto_prev";
-            "<C-Right>" = "goto_next";
-          };
-
-          lspBuf = {
-            gd = "definition";
-            gD = "references";
-            gt = "type_definition";
-            gi = "implementation";
-            K = "hover";
-            "<F2>" = "rename";
-          };
+        diagnostic = {
+          "<C-Left>" = "goto_prev";
+          "<C-Right>" = "goto_next";
         };
 
-        servers = {
-          nil-ls = {
-            enable = true;
-
-            settings = {
-              formatting.command = [
-                "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"
-              ];
-            };
-          };
-          bashls.enable = true;
-
-          clangd.enable = true;
-          cmake.enable = true;
-
-          dartls.enable = true;
-
-          cssls.enable = true;
-          denols.enable = true;
+        lspBuf = {
+          gd = "definition";
+          gD = "references";
+          gt = "type_definition";
+          gi = "implementation";
+          K = "hover";
+          "<F2>" = "rename";
         };
       };
 
-      lsp-format = {
-        enable = true;
+      servers = {
+        nil-ls = {
+          enable = true;
+
+          settings = {
+            formatting.command = [
+              "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"
+            ];
+          };
+        };
+        bashls.enable = true;
+
+        clangd.enable = true;
+        cmake.enable = true;
+
+        dartls.enable = true;
+
+        cssls.enable = true;
+        denols.enable = true;
       };
+    };
+
+    lsp-format = {
+      enable = true;
+    };
   };
 }
