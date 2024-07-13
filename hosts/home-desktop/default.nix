79949ec3fc8d2829f56b6d04cd450d0594717740
soriphoono <soriphoono@gmail.com>
2024-07-13 13:10:52 -0500

    Updated flake

--- hosts/home-desktop/default.nix
+++ hosts/home-desktop/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
 
@@ -17,4 +17,24 @@
 
     ../../modules/services/pipewire.nix
   ];
+
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver
+    vaapiVdpau
+    libvdpau-va-gl
+
+    intel-compute-runtime
+
+    rocmPackages.clr.icd
+  ];
+
+  systemd.tmpfiles.rules = 
+    [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
+
+  environment.sessionVariables = {
+    KWIN_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card2";
+
+    LIBVA_DRIVER_NAME = "iHD";
+    VDPAU_DRIVER = "va_gl";
+  };
 }
