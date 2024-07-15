c30cf172fcad0f1bbf38610db63d18957df46483
soriphoono <soriphoono@gmail.com>
2024-07-15 13:42:30 -0500

    chore: Update default.nix to include i18n configuration

--- systems/x86_64-linux/wsl/default.nix
+++ systems/x86_64-linux/wsl/default.nix
@@ -1,28 +1,4 @@
 { lib, config, pkgs, ... }: {
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
-  time.timeZone = lib.mkDefault "America/Chicago";
-
-  environment.systemPackages = with pkgs; [
-    coreutils
-
-    wget
-  ];
-
   programs = {
     nix-index-database.comma.enable = true;
 
