f09ce0aa5e96e81825e433c90a476ed954cfbf97
soriphoono <soriphoono@gmail.com>
2024-07-18 16:41:13 -0500

    Added workspace bindings

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -15,25 +15,35 @@ in {
         bind = [
           "$mod, B, exec, firefox"
           "$mod, RETURN, exec, alacritty"
-        ];
+        ] ++ (builtins.concatLists (builtins.genList (
+          x: let
+            ws = let
+              c = (x + 1) / 10;
+            in
+              builtins.toString (x + 1 - (c * 10));
+          in [
+            "$mod, ${ws}, workspace, ${toString (x + 1)}"
+            "$mod SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
+          ]
+        ) 10));
       };
     };
 
     programs = {
-    alacritty = {
-      enable = true;
+      alacritty = {
+        enable = true;
 
-      settings = {
-        window = {
-          blur = true;
+        settings = {
+          window = {
+            blur = true;
 
-          decorations = "None";
-          startup_mode = "Maximized";
+            decorations = "None";
+            startup_mode = "Maximized";
+          };
+
+          cursor.style = "Beam";
         };
-
-        cursor.style = "Beam";
       };
-    };
     };
   };
 }
