1fd89c9d9343ff037cbadff5e2cae64f5520dcd3
soriphoono <soriphoono@gmail.com>
2024-08-05 12:55:58 -0500

    Updated vscode overlay

--- overlays/vscode/default.nix
+++ overlays/vscode/default.nix
@@ -1,8 +1,8 @@
 { ... }: self: super: {
-  discord = super.discord.overrideAttrs (
+  vscode = super.vscode.overrideAttrs (
     _: {
       src = builtins.fetchTarball {
-        url = "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64";
+        url = "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64";
         sha256 = "0k843yfnxj1hnw3bmki13625ywfy3k0pf5xlng0xmaacz2jf3lkk";
       };
     }
