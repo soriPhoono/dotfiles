8655af2ba352b294bc744eb6a066265041947bc9
soriphoono <soriphoono@gmail.com>
2024-08-15 15:53:38 -0500

    Added dedicated archiving tools

---
+++ modules/home/terminal/programs/archiving/default.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, ... }:
+let cfg = config.terminal.programs.archiving;
+in {
+  options = {
+    terminal.programs.archiving.enable = lib.mkEnableOption "Enable archiving programs";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      zip
+      unzip
+      p7zip
+      unrar
+
+      yt-dlp
+      spotdl
+      ytmdl
+    ];
+  };
+}
--- modules/home/terminal/programs/default.nix
+++ modules/home/terminal/programs/default.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     imagemagick
-
-    yt-dlp
-    spotdl
-    ytmdl
   ];
 
   programs.git = {
