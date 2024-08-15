50016a284a4b4a6b1127fdc44668b4d087f96046
soriphoono <soriphoono@gmail.com>
2024-08-15 18:19:06 -0500

    updated nvim

--- modules/home/terminal/programs/development/nvim/default.nix
+++ modules/home/terminal/programs/development/nvim/default.nix
@@ -2,6 +2,25 @@
 let cfg = config.terminal.programs.development;
 in {
   config = lib.mkIf cfg.enable {
-    programs.nixvim.enable = true;
+    programs.nixvim = {
+      enable = true;
+      defaultEditor = true;
+
+      colorschemes.catppuccin = {
+        enable = true;
+
+        settings = {
+          flavour = "mocha";
+
+          transparent_background = true;
+        };
+      };
+
+      editorconfig.enable = true;
+
+      plugins = {
+        airline.enable = true;
+      };
+    };
   };
 }
