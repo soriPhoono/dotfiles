4a478f631e5703d791ed0dfc82901d07c04be65e
soriphoono <soriphoono@gmail.com>
2024-08-18 23:56:42 -0500

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,26 +1,24 @@
 {
   description = "Personal dotfiles for NixOS";
 
-  outputs = 
-    inputs@{
-      self,
-      nixpkgs,
-      flake-parts,
-      ...
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      
+
       imports = [
         ./hosts
       ];
-      
-      perSystem = 
-        {
-          inputs',
-          pkgs,
-          system,
-          ...
+
+      perSystem =
+        { inputs'
+        , pkgs
+        , system
+        , ...
         }:
         {
           _module.args.pkgs = import inputs.nixpkgs {
@@ -37,21 +35,21 @@
 
   inputs = {
     # Core imports
-    
+
     flake-parts.url = "github:hercules-ci/flake-parts";
-    
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # System-specific imports
-    
-    nixos-wsl = { 
+
+    nixos-wsl = {
       url = "github:nix-community/nixos-wsl";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # User environment imports
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
 
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,42 +1,44 @@
 { self, inputs, ... }: {
-  flake.nixosConfigurations = 
-    let 
-      mkHost = {
-        hostname,
-        username,
-        nixpkgs ? inputs.nixpkgs,
-        system ? "x86_64-linux",
-        defaultModules ? true,
-        hmEnable ? true,
-        extraModules ? [],
-      }: nixpkgs.lib.nixosSystem {
-        inherit system;
+  flake.nixosConfigurations =
+    let
+      mkHost =
+        { hostname
+        , username
+        , nixpkgs ? inputs.nixpkgs
+        , system ? "x86_64-linux"
+        , defaultModules ? true
+        , hmEnable ? true
+        , extraModules ? [ ]
+        ,
+        }: nixpkgs.lib.nixosSystem {
+          inherit system;
 
-        specialArgs = {
-          inherit self inputs username;
-        };
+          specialArgs = {
+            inherit self inputs username;
+          };
 
-        modules = [
-          
-        ] ++ nixpkgs.lib.optional hmEnable [
-          inputs.home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
+          modules = [
 
-              backupFileExtension = ".bak";
+          ] ++ nixpkgs.lib.optional hmEnable [
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
 
-              extraSpecialArgs = {
-                inherit inputs;
+                backupFileExtension = ".bak";
+
+                extraSpecialArgs = {
+                  inherit inputs;
+                };
+
+                users.${username} = ../homes/${username};
               };
+            }
+          ] ++ nixpkgs.lib.optional defaultModules [
 
-              users.${username} = ../homes/${username};
-            };
-          }
-        ] ++ nixpkgs.lib.optional defaultModules [
-
-        ] ++ extraModules;
-      };
+          ] ++ extraModules;
+        };
     in
     {
       wsl = mkHost {
