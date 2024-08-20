bdd3144c0330816352b324a966bb6f5b45bc337b
soriphoono <soriphoono@gmail.com>
2024-08-20 11:10:16 -0500

    Formatting

--- homes/modules/themes/catppuccin.nix
+++ homes/modules/themes/catppuccin.nix
@@ -1,49 +1,49 @@
 {
   stylix = {
-      enable = true;
+    enable = true;
 
-      image = ../../../../assets/wallpapers/soulful_anime.jpg;
+    image = ../../../../assets/wallpapers/soulful_anime.jpg;
 
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-      cursor = {
-        package = pkgs.catppuccin-cursors.mochaTeal;
-        size = 32;
-        name = "catppuccin-mocha-teal-cursors";
-      };
-
-      fonts = {
-        serif = {
-          package = pkgs.nerdfonts;
-          name = "AurulentSansM Nerd Font Propo";
-        };
-
-        sansSerif = {
-          package = pkgs.nerdfonts;
-          name = "AurulentSansM Nerd Font Propo";
-        };
-
-        monospace = {
-          package = pkgs.nerdfonts;
-          name = "JetBrainsMono Nerd Font Mono";
-        };
-
-        emoji = {
-          package = pkgs.noto-fonts-emoji;
-          name = "Noto Color Emoji";
-        };
-
-        sizes =
-          let
-            default_font = 14;
-            focus_font = 16;
-          in
-          {
-            applications = focus_font;
-            desktop = focus_font;
-            popups = default_font;
-            terminal = focus_font;
-          };
-      };
+    cursor = {
+      package = pkgs.catppuccin-cursors.mochaTeal;
+      size = 32;
+      name = "catppuccin-mocha-teal-cursors";
     };
+
+    fonts = {
+      serif = {
+        package = pkgs.nerdfonts;
+        name = "AurulentSansM Nerd Font Propo";
+      };
+
+      sansSerif = {
+        package = pkgs.nerdfonts;
+        name = "AurulentSansM Nerd Font Propo";
+      };
+
+      monospace = {
+        package = pkgs.nerdfonts;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes =
+        let
+          default_font = 14;
+          focus_font = 16;
+        in
+        {
+          applications = focus_font;
+          desktop = focus_font;
+          popups = default_font;
+          terminal = focus_font;
+        };
+    };
+  };
 }
