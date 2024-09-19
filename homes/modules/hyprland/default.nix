fb18a65a4ad4c1238bcb403622b4d375031f10f9
soriphoono <soriphoono@gmail.com>
2024-09-19 16:15:22 -0500

    Typos

--- homes/modules/hyprland/default.nix
+++ homes/modules/hyprland/default.nix
@@ -4,7 +4,7 @@
 }: {
   imports = [
     ../desktop
-    ./supporting.nix
+    ./supporting
     
     ./config
   ];
--- homes/modules/hyprland/supporting/default.nix
+++ homes/modules/hyprland/supporting/default.nix
@@ -2,5 +2,5 @@
   imports = [
     ./alacritty.nix
     ./dunst.nix
-  ]
+  ];
 }
