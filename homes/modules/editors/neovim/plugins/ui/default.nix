8106400b9e8d11e336b7200d589af2ef4f6a6a46
soriphoono <soriphoono@gmail.com>
2024-09-26 16:13:00 -0500

    Updated treesitter and cmp extension modules

--- homes/modules/editors/neovim/plugins/ui/default.nix
+++ homes/modules/editors/neovim/plugins/ui/default.nix
@@ -1,5 +1,13 @@
 {
-  imports = [ ./neo-tree.nix ./lualine.nix ./telescope.nix ./gitsigns.nix ./toggleterm.nix ];
+  imports = [
+    ./cmp.nix
+    ./treesitter.nix
+    ./neo-tee.nix
+    ./lualine.nix
+    ./telescope.nix
+    ./gitsigns.nix
+    ./toggleterm.nix
+  ];
 
   programs.nixvim.plugins.which-key.enable = true;
 }
