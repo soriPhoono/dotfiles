23d1e75a69e2b04fb103deb00d6c8372930b04c7
soriphoono <soriphoono@gmail.com>
2024-09-26 18:18:18 -0500

    Added hotkeys for fugitive

--- homes/modules/editors/neovim/plugins/util/fugitive.nix
+++ homes/modules/editors/neovim/plugins/util/fugitive.nix
@@ -1,1 +1,12 @@
-{ programs.nixvim = { plugins.fugitive = { enable = true; }; }; }
+{
+  programs.nixvim = {
+    keymaps = builtins.map (v: v // { options.silent = true; }) [{
+      key = "<leader>ga";
+      action = "<cmd>G add %<CR>";
+      mode = [ "n" ];
+      options.desc = "Add the current file to the index";
+    }];
+
+    plugins.fugitive = { enable = true; };
+  };
+}
