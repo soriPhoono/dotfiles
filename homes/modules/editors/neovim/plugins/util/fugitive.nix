f8dca6c7c209ee59380b3a7bb4a98a195d733a4b
soriphoono <soriphoono@gmail.com>
2024-09-26 18:20:45 -0500

    Added commit hotkey

--- homes/modules/editors/neovim/plugins/util/fugitive.nix
+++ homes/modules/editors/neovim/plugins/util/fugitive.nix
@@ -1,11 +1,20 @@
 {
   programs.nixvim = {
-    keymaps = builtins.map (v: v // { options.silent = true; }) [{
-      key = "<leader>ga";
-      action = "<cmd>G add %<CR>";
-      mode = [ "n" ];
-      options.desc = "Add the current file to the index";
-    }];
+    keymaps = builtins.map (v: v // { options.silent = true; }) [
+      {
+        key = "<leader>ga";
+        action = "<cmd>G add %<CR>";
+        mode = [ "n" ];
+        options.desc = "Add the current file to the index";
+      }
+      {
+        key = "<leader>gc";
+        action = "<cmd>G commit<CR>";
+        mode = [ "n" ];
+        options.desc =
+          "Commit the current altered working tree to the repository";
+      }
+    ];
 
     plugins.fugitive = { enable = true; };
   };
