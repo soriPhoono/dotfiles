d8aa39b20f3c8ad8cb1d046663aba6ba982de833
soriphoono <soriphoono@gmail.com>
2024-09-26 13:19:32 -0500

    Migrated telescope keymaps to internal system

--- homes/modules/editors/neovim/config/autocmd.nix
+++ homes/modules/editors/neovim/config/autocmd.nix
@@ -10,17 +10,8 @@
       command = "wincmd L";
     }
     {
-      event = [ "FileType" ];
-      pattern = "TelescopePrompt";
-      command = "inoremap <buffer><silent> <ESC> <ESC>:close!<CR>";
-    }
-    {
       event = "FileType";
-      pattern = [
-        "tex"
-        "latex"
-        "markdown"
-      ];
+      pattern = [ "tex" "latex" "markdown" ];
       command = "setlocal spell spelllang=en";
     }
   ];
--- homes/modules/editors/neovim/plugins/ui/telescope.nix
+++ homes/modules/editors/neovim/plugins/ui/telescope.nix
@@ -1,44 +1,30 @@
 {
   programs = {
     nixvim = {
-      keymaps = [
-        {
-          action = "<cmd>Telescope find_files<CR>";
-          key = "<C-p>";
-          mode = [ "n" ];
-          options = {
-            silent = true;
-          };
-        }
-        {
-          action = "<cmd>Telescope live_grep<CR>";
-          key = "<leader>fg";
-          mode = [ "n" ];
-          options = {
-            silent = true;
-          };
-        }
-        {
-          action = "<cmd>Telescope buffers<CR>";
-          key = "<leader>fb";
-          mode = [ "n" ];
-          options = {
-            silent = true;
-          };
-        }
-        {
-          action = "<cmd>Telescope help_tags<CR>";
-          key = "<leader>fh";
-          mode = [ "n" ];
-          options = {
-            silent = true;
-          };
-        }
-      ];
-
       plugins = {
         web-devicons.enable = true;
-        telescope.enable = true;
+        telescope = {
+          enable = true;
+
+          keymaps = builtins.mapAttrs (n: v: v // { options.silent = true; }) {
+            "<leader>fh" = {
+              action = "<cmd>Telescope help_tags<CR>";
+              options = { desc = "Telescope find help tags"; };
+            };
+            "<leader>fb" = {
+              action = "<cmd>Telescope buffers<CR>";
+              options = { desc = "Telescope find buffer"; };
+            };
+            "<leader>ff" = {
+              action = "<cmd>Telescope find_files<CR>";
+              options = { desc = "Telescope find_files"; };
+            };
+            "<leader>fg" = {
+              action = "<cmd>Telescope live_grep<CR>";
+              options = { desc = "Telescope live_grep"; };
+            };
+          };
+        };
       };
     };
 
