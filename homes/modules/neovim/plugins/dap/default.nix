7452467398e0d97733ce33f7234e6b7d411a56e4
soriphoono <soriphoono@gmail.com>
2024-09-26 22:37:59 -0500

    Removed dapui, no exposed commands to link against

--- homes/modules/neovim/plugins/dap/default.nix
+++ homes/modules/neovim/plugins/dap/default.nix
@@ -5,7 +5,7 @@
     keymaps = builtins.map (v: v // { options.silent = true; }) [
       {
         key = "<F5>";
-        action = "<cmd>lua require('dapui').open<CR><cmd>DapContinue<CR>";
+        action = "<cmd>DapContinue<CR>";
         mode = [ "n" ];
         options.desc = "Continue or activate debugging session";
       }
@@ -29,62 +29,6 @@
       }
     ];
 
-    plugins.dap = {
-      enable = true;
-
-      extensions = {
-        dap-ui = {
-          enable = true;
-
-          layouts = [
-            {
-              elements = [
-                {
-                  id = "scopes";
-                  size = 0.25;
-                }
-                {
-                  id = "breakpoints";
-                  size = 0.25;
-                }
-                {
-                  id = "stacks";
-                  size = 0.25;
-                }
-                {
-                  id = "watches";
-                  size = 0.25;
-                }
-              ];
-              position = "left";
-              size = 40;
-            }
-            {
-              elements = [
-                {
-                  id = "repl";
-                  size = 0.5;
-                }
-                {
-                  id = "console";
-                  size = 0.5;
-                }
-              ];
-              position = "bottom";
-              size = 10;
-            }
-          ];
-
-          mappings = {
-            edit = "<leader>de";
-            expand = "<CR>";
-            open = "<leader>do";
-            remove = "<leader>dd";
-            repl = "<leader>dr";
-            toggle = "<leader>dt";
-          };
-        };
-      };
-    };
+    plugins.dap.enable = true;
   };
 }
