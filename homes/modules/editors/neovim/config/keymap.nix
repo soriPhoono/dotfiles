1566e3def5b55bb882d1a79f3a74857bdd7a85b8
soriphoono <soriphoono@gmail.com>
2024-09-26 21:11:32 -0500

    Updated dap

--- homes/modules/editors/neovim/config/keymap.nix
+++ homes/modules/editors/neovim/config/keymap.nix
@@ -1,14 +1,14 @@
 {
   programs.nixvim.keymaps = builtins.map (v: v // { options.silent = true; }) [
     {
-      key = "<leader>ww";
+      key = "<leader>w";
       action = "<cmd>write<CR>";
       mode = [ "n" ];
       options.desc = "Save to disk";
     }
     {
-      key = "<leader>wq";
-      action = "<cmd>wq<CR>";
+      key = "<leader>q";
+      action = "<cmd>quit<CR>";
       mode = [ "n" ];
       options.desc = "Save and quit";
     }
--- homes/modules/editors/neovim/plugins/ui/dap.nix
+++ homes/modules/editors/neovim/plugins/ui/dap.nix
@@ -1,62 +1,100 @@
 {
-  programs.nixvim.plugins.dap = {
-    enable = true;
+  programs.nixvim = {
+    keymaps = builtins.map (v: v // { options.silent = true; }) [
+      {
+        key = "<F5>";
+        action = "<cmd>DapContinue<CR>";
+        mode = [ "n" ];
+        options.desc = "Continue or activate debugging session";
+      }
+      {
+        key = "<leader>db";
+        action = "<cmd>DapToggleBreakpoint<CR>";
+        mode = [ "n" ];
+        options.desc = "Toggle breakpoint";
+      }
+      {
+        key = "<leader>dso";
+        action = "<cmd>DapStepOver";
+        mode = [ "n" ];
+        options.desc = "Step over symbol";
+      }
+      {
+        key = "<leader>dsi";
+        action = "<cmd>DapStepInto";
+        mode = [ "n" ];
+        options.desc = "Step into symbol";
+      }
+    ];
 
-    adapters.executables = {
-      "nix" = {
-        command = "nix";
-        args = [ "flake" "check" ];
+    plugins.dap = {
+      enable = true;
+
+      adapters.executables = {
+        "nix" = {
+          command = "nix";
+          args = [ "flake" "check" ];
+        };
       };
-    };
 
-    configurations."nix" = [{
-      name = "Check nix flake";
-      request = "launch";
-      type = "nix";
-    }];
+      configurations."nix" = [{
+        name = "Check nix flake";
+        request = "launch";
+        type = "nix";
+      }];
 
-    extensions = {
-      dap-ui = {
-        enable = true;
+      extensions = {
+        dap-ui = {
+          enable = true;
 
-        layouts = [
-          {
-            elements = [
-              {
-                id = "scopes";
-                size = 0.25;
-              }
-              {
-                id = "breakpoints";
-                size = 0.25;
-              }
-              {
-                id = "stacks";
-                size = 0.25;
-              }
-              {
-                id = "watches";
-                size = 0.25;
-              }
-            ];
-            position = "left";
-            size = 40;
-          }
-          {
-            elements = [
-              {
-                id = "repl";
-                size = 0.5;
-              }
-              {
-                id = "console";
-                size = 0.5;
-              }
-            ];
-            position = "bottom";
-            size = 10;
-          }
-        ];
+          layouts = [
+            {
+              elements = [
+                {
+                  id = "scopes";
+                  size = 0.25;
+                }
+                {
+                  id = "breakpoints";
+                  size = 0.25;
+                }
+                {
+                  id = "stacks";
+                  size = 0.25;
+                }
+                {
+                  id = "watches";
+                  size = 0.25;
+                }
+              ];
+              position = "left";
+              size = 40;
+            }
+            {
+              elements = [
+                {
+                  id = "repl";
+                  size = 0.5;
+                }
+                {
+                  id = "console";
+                  size = 0.5;
+                }
+              ];
+              position = "bottom";
+              size = 10;
+            }
+          ];
+
+          mappings = {
+            edit = "<leader>de";
+            expand = "<CR>";
+            open = "<leader>do";
+            remove = "<leader>dd";
+            repl = "<leader>dr";
+            toggle = "<leader>dt";
+          };
+        };
       };
     };
   };
