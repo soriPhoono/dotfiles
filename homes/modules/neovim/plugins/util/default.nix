fbf1f26243fae40c74baeb32145080715b05a1ed
soriphoono <soriphoono@gmail.com>
2024-09-27 14:49:22 -0500

    Added rename hotkey

--- homes/modules/neovim/plugins/util/default.nix
+++ homes/modules/neovim/plugins/util/default.nix
@@ -3,12 +3,20 @@
     [ ./commentary.nix ./autopairs.nix ./gitsigns.nix ./toggleterm.nix ];
 
   programs.nixvim = {
-    keymaps = builtins.map (v: v // { options.silent = true; }) [{
-      key = "<leader>g";
-      action = "<cmd>LazyGit<CR>";
-      mode = [ "n" ];
-      options.desc = "Open LazyGit";
-    }];
+    keymaps = builtins.map (v: v // { options.silent = true; }) [
+      {
+        key = "<leader>g";
+        action = "<cmd>LazyGit<CR>";
+        mode = [ "n" ];
+        options.desc = "Open LazyGit";
+      }
+      {
+        key = "<F2>";
+        action = "<cmd>IncRename <cword><CR>";
+        mode = [ "n" ];
+        options.desc = "Rename current token";
+      }
+    ];
 
     plugins = {
       lazygit.enable = true;
