acdc3d8bcf62e6c4eadeef83edf69867d05a703a
soriphoono <soriphoono@gmail.com>
2024-09-26 22:20:45 -0500

    Added basic python tooling to core module

--- homes/modules/core/utils.nix
+++ homes/modules/core/utils.nix
@@ -1,9 +1,17 @@
 { pkgs, ... }: {
   imports = [ ../editors/helix ../editors/neovim ];
 
-  home.shellAliases = with pkgs; {
-    df = "${duf}/bin/duf";
-    du = "${dua}/bin/dua";
+  home = {
+    packages = with pkgs; [
+      # python
+      python3
+      python312Packages.pip
+    ];
+
+    shellAliases = with pkgs; {
+      df = "${duf}/bin/duf";
+      du = "${dua}/bin/dua";
+    };
   };
 
   programs = {
