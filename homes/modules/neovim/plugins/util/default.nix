bd02c0ce5313da8295420231e3c9b7f41d0fc816
soriphoono <soriphoono@gmail.com>
2024-09-27 15:17:59 -0500

    removed inc rename

--- homes/modules/neovim/plugins/util/default.nix
+++ homes/modules/neovim/plugins/util/default.nix
@@ -3,33 +3,13 @@
     [ ./commentary.nix ./autopairs.nix ./gitsigns.nix ./toggleterm.nix ];
 
   programs.nixvim = {
-    keymaps = builtins.map (v: v // { options.silent = true; }) [
-      {
-        key = "<leader>g";
-        action = "<cmd>LazyGit<CR>";
-        mode = [ "n" ];
-        options.desc = "Open LazyGit";
-      }
-      {
-        key = "<F2>";
-        action =
-          # lua
-          ''
-            function()
-              return ":IncRename " .. vim.fn.expand("<cword>")
-            end
-          '';
-        mode = [ "n" ];
-        options = {
-          desc = "Rename current token";
-          expr = true;
-        };
-      }
-    ];
+    keymaps = builtins.map (v: v // { options.silent = true; }) [{
+      key = "<leader>g";
+      action = "<cmd>LazyGit<CR>";
+      mode = [ "n" ];
+      options.desc = "Open LazyGit";
+    }];
 
-    plugins = {
-      lazygit.enable = true;
-      inc-rename.enable = true;
-    };
+    plugins = { lazygit.enable = true; };
   };
 }
