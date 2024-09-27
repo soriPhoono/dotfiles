3e0cc6da5f1064f93f6f0b9ee8c89a14c2a1770b
soriphoono <soriphoono@gmail.com>
2024-09-27 14:41:10 -0500

    chore: updated keybinds
    Updated keybinds for lazygit and toggleterm

--- homes/modules/neovim/plugins/util/default.nix
+++ homes/modules/neovim/plugins/util/default.nix
@@ -2,5 +2,14 @@
   imports =
     [ ./commentary.nix ./autopairs.nix ./gitsigns.nix ./toggleterm.nix ];
 
-  programs.nixvim.plugins.lazygit.enable = true;
+  programs.nixvim = {
+    keymaps = builtins.map (v: v // { options.silent = true; }) [{
+      key = "<leader>g";
+      action = "<cmd>LazyGit<CR>";
+      mode = [ "n" ];
+      options.desc = "Open LazyGit";
+    }];
+
+    plugins.lazygit.enable = true;
+  };
 }
