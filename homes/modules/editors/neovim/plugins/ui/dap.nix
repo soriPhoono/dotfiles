c8c43cc32708b51b56afe69f776f505125dd6852
soriphoono <soriphoono@gmail.com>
2024-09-26 21:01:04 -0500

    Updated dap ui

--- homes/modules/editors/neovim/plugins/ui/dap.nix
+++ homes/modules/editors/neovim/plugins/ui/dap.nix
@@ -22,14 +22,36 @@
         layouts = [
           {
             elements = [
-
+              {
+                id = "scopes";
+                size = 0.25;
+              }
+              {
+                id = "breakpoints";
+                size = 0.25;
+              }
+              {
+                id = "stacks";
+                size = 0.25;
+              }
+              {
+                id = "watches";
+                size = 0.25;
+              }
             ];
             position = "left";
             size = 40;
           }
           {
             elements = [
-              # TODO: finish this
+              {
+                id = "repl";
+                size = 0.5;
+              }
+              {
+                id = "console";
+                size = 0.5;
+              }
             ];
             position = "bottom";
             size = 10;
