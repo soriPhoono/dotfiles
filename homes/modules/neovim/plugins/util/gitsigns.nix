7c015eab0362e06fd5fa80db02ebf968ccc82534
soriphoono <soriphoono@gmail.com>
2024-09-27 15:21:59 -0500

    Removed unused keybind

--- homes/modules/neovim/plugins/util/gitsigns.nix
+++ homes/modules/neovim/plugins/util/gitsigns.nix
@@ -1,12 +1,5 @@
 {
   programs.nixvim = {
-    keymaps = builtins.map (v: v // { options.silent = true; }) [{
-      key = "<leader>gt";
-      action = "<cmd>Gitsigns toggle_signs<CR>";
-      mode = [ "n" ];
-      options.desc = "Toggle git gutter signs";
-    }];
-
     plugins.gitsigns = {
       enable = true;
 
