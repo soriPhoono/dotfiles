cc72d13a84971c205d366f7cf15e97b3b276cdaa
soriphoono <soriphoono@gmail.com>
2024-09-30 13:46:59 -0500

    Wrote basic bar

--- ags/config.js
+++ ags/config.js
@@ -1,1 +1,13 @@
+const Bar = () =>
+  Widget.Window({
+    name: "bar",
+    anchor: ["top", "left", "right"],
+    child: Widget.Label().poll(
+      1000,
+      (label) => (label.label = Utils.exec("date")),
+    ),
+  });
 
+App.config({
+  windows: [Bar(0)],
+});
