4f30de606c120b09e5ae4c4dd1a3d2da0ff03f70
soriphoono <soriphoono@gmail.com>
2024-10-01 19:23:04 -0500

    Adjusted keymap

--- homes/modules/core/neovim/config/keymap.nix
+++ homes/modules/core/neovim/config/keymap.nix
@@ -26,43 +26,6 @@ builtins.map (v: v // { options.silent = true; }) [
   }
 
   {
-    key = "<leader>te";
-    action = "<cmd>tabedit<CR>";
-    mode = [ "n" ];
-    options.desc = "Open new tab";
-  }
-  {
-    key = "<leader>tn";
-    action = "<cmd>tabnext<CR>";
-    mode = [ "n" ];
-    options.desc = "Go to next tab";
-  }
-  {
-    key = "<leader>tp";
-    action = "<cmd>tabprev<CR>";
-    mode = [ "n" ];
-    options.desc = "Go to previous tab";
-  }
-  {
-    key = "<leader>tq";
-    action = "<cmd>tabclose<CR>";
-    mode = [ "n" ];
-    options.desc = "Close current tab";
-  }
-
-  {
-    key = "<leader>wh";
-    action = "<cmd>split<CR>";
-    mode = [ "n" ];
-    options.desc = "Split current window horizontally";
-  }
-  {
-    key = "<leader>wv";
-    action = "<cmd>vsplit<CR>";
-    mode = [ "n" ];
-    options.desc = "Split current window vertically";
-  }
-  {
     key = "<leader>wq";
     action = "<cmd>close<CR>";
     mode = [ "n" ];
