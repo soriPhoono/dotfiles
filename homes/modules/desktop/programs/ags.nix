b82dd61aebf7d246132e478238cc43750cbefe31
soriphoono <soriphoono@gmail.com>
2024-10-02 13:23:31 -0500

    Updated ags

--- homes/modules/desktop/programs/ags.nix
+++ homes/modules/desktop/programs/ags.nix
@@ -1,10 +1,10 @@
 { inputs, lib, pkgs, config, ... }:
-let cfg = config.desktop.ags;
+let cfg = config.desktop.programs.ags;
 in {
   imports = [ inputs.ags.homeManagerModules.default ];
 
   options = {
-    desktop.ags.enable = lib.mkEnableOption "Enable ags wayland shell";
+    desktop.programs.ags.enable = lib.mkEnableOption "Enable ags wayland shell";
   };
 
   config = lib.mkIf cfg.enable {
