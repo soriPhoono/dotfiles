24ebf6c303c5420df4c562dab8112c004049cd96
soriphoono <soriphoono@gmail.com>
2024-10-02 12:37:40 -0500

    Updated handling of importing user environments to nixos

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,51 +1,49 @@
 { self, inputs, ... }: {
-  flake.nixosConfigurations =
-    let
-      inherit (inputs.nixpkgs) lib;
+  flake.nixosConfigurations = let
+    inherit (inputs.nixpkgs) lib;
 
-      mkHost =
-        { hostname, username, systemModules ? [ ], system ? "x86_64-linux" }:
-        lib.nixosSystem {
-          inherit system;
+    mkHost =
+      { hostname, username, systemModules ? [ ], system ? "x86_64-linux" }:
+      lib.nixosSystem {
+        inherit system;
 
-          specialArgs = { inherit self inputs lib username; };
+        specialArgs = { inherit self inputs lib username; };
 
-          modules = with inputs;
-            [
-              { networking.hostName = "${hostname}"; }
+        modules = with inputs;
+          [
+            { networking.hostName = "${hostname}"; }
 
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
 
-                  backupFileExtension = "~";
+                backupFileExtension = "~";
 
-                  sharedModules = [ self.homeManagerModules.default ];
-                  extraSpecialArgs = { inherit inputs username; };
+                sharedModules = [ self.homeManagerModules.default ];
+                extraSpecialArgs = { inherit inputs username; };
 
-                  users.${username} = ../homes/${username};
-                };
-              }
+                users.${username} = ../homes/${username}_${hostname};
+              };
+            }
 
-              self.nixosModules.default
-            ] ++ systemModules;
-        };
-    in
-    {
-      wsl = mkHost {
-        hostname = "wsl";
-        username = "soriphoono";
-
-        systemModules = [ ./wsl ];
+            self.nixosModules.default
+          ] ++ systemModules;
       };
+  in {
+    wsl = mkHost {
+      hostname = "wsl";
+      username = "soriphoono";
 
-      zephyrus = mkHost {
-        hostname = "zephyrus";
-        username = "soriphoono";
-
-        systemModules = [ ./zephyrus ];
-      };
+      systemModules = [ ./wsl ];
     };
+
+    zephyrus = mkHost {
+      hostname = "zephyrus";
+      username = "soriphoono";
+
+      systemModules = [ ./zephyrus ];
+    };
+  };
 }
