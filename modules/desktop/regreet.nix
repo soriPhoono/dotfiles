087731c887cd02aee68197217517b38b38dcbbed
soriphoono <soriphoono@gmail.com>
2024-10-03 15:44:55 -0500

    Finished regreet v1

--- modules/desktop/regreet.nix
+++ modules/desktop/regreet.nix
@@ -1,10 +1,46 @@
-{ lib, config, ... }:
+{ lib, pkgs, config, ... }:
 let cfg = config.desktop.regreet;
 in {
+  imports = [ ./greetd.nix ];
+
   options = {
     desktop.regreet.enable =
       lib.mkEnableOption "Enable regreet display manager";
   };
 
-  config = lib.mkIf cfg.enable { programs.regreet.enable = true; };
+  config = lib.mkIf cfg.enable {
+    programs.regreet = {
+      enable = true;
+
+      cursorTheme = {
+        package = pkgs.catppuccin-cursors.mochaTeal;
+        name = "catppuccin-mocha-teal-cursors";
+      };
+
+      font = {
+        package = pkgs.nerdfonts;
+        name = "AurulentSansM Nerd Font Propo";
+      };
+
+      iconTheme = {
+        package = pkgs.papirus-icon-theme;
+        name = "Papirus-Dark";
+      };
+
+      theme = {
+        package = pkgs.catppuccin-gtk.overrideAttrs {
+          accents = "teal";
+          variant = "mocha";
+        };
+        name = "catppuccin-mocha-teal-standard+default";
+      };
+
+      settings = {
+        background = {
+          path = ../../assets/catppuccin-mountain.jpg;
+          fit = "Contain";
+        };
+      };
+    };
+  };
 }
