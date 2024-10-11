abd7f3186d06bb6b503025aa6998b10382845d0c
soriphoono <soriphoono@gmail.com>
2024-10-10 20:59:26 -0500

    Updated desktop and discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -2,7 +2,7 @@ final: prev: {
   discord = prev.discord.overrideAttrs (_: {
     src = builtins.fetchTarball {
       url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-      sha256 = "1vqm2d8j4pjn42darjpvfs6f81rias8d7rp9dw274ih78c5a9840";
+      sha256 = "0njwcnrn2br47dzqvmlazcmf63bblx68775f0kv8djwxfvg977im";
     };
   });
 }
