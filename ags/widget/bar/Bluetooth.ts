928a861817f74bf504faa3440f3813af10e522d8
soriphoono <soriphoono@gmail.com>
2024-10-23 12:52:51 -0500

    Updated handling of bluetooth bar monitor

--- ags/widget/bar/Bluetooth.ts
+++ ags/widget/bar/Bluetooth.ts
@@ -1,37 +1,31 @@
+import HoverRevealer from "lib/HoverRevealer"
+
 const bluetooth = await Service.import('bluetooth')
 
-export default () => Widget.EventBox({
+export default () => HoverRevealer({
+  class_name: 'bluetooth',
+
   on_primary_click: () => Utils.execAsync('blueberry').catch(console.error),
   on_secondary_click: () => bluetooth.toggle(),
-
-  child: Widget.Box({
-    children: [
-      Widget.Icon({
-        icon: bluetooth.bind('connected_devices').as(connected_devices => connected_devices.length > 0
-          ? 'bluetooth-symbolic'
-          : 'bluetooth-disconnected-symbolic')
-      }),
-      Widget.Revealer({
-        reveal_child: bluetooth.bind('connected_devices')
-          .as(connected_devices => connected_devices.length > 0),
-        transition_duration: 1000,
-        transition: 'slide_right',
-
-        child: Widget.Box({
-          children: bluetooth.bind('connected_devices').as(devices => devices.map(device =>
-            Widget.Box({
-              children: [
-                Widget.Icon({
-                  icon: device.bind('icon_name').as(icon_name => `${icon_name}-symbolic`)
-                }),
-                Widget.Label({
-                  label: device.bind('battery_percentage').as(percentage => `${percentage}%`)
-                })
-              ]
-            })
-          ))
-        })
+},
+  Widget.Icon({
+    icon: bluetooth.bind('connected_devices').as(connected_devices => connected_devices.length > 0
+      ? 'bluetooth-symbolic'
+      : 'bluetooth-disconnected-symbolic')
+  }),
+  Widget.Box({
+    children: bluetooth.bind('connected_devices').as(devices => devices.map(device =>
+      Widget.Box({
+        children: [
+          Widget.Icon({
+            icon: device.bind('icon_name').as(icon_name => `${icon_name}-symbolic`)
+          }),
+          Widget.Label({
+            label: device.bind('battery_percentage').as(percentage => `${percentage}%`)
+          })
+        ]
       })
-    ]
+    ))
   })
-})
+)
+
