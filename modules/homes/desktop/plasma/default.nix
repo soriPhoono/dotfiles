2904281a419081557df9901e6dec0216c120e79e
soriphoono <soriphoono@gmail.com>
2024-10-26 00:00:46 -0500

    see previous

--- modules/homes/desktop/plasma/default.nix
+++ modules/homes/desktop/plasma/default.nix
@@ -2,6 +2,23 @@
   programs.plasma = {
     enable = true;
 
+    panels = [
+      {
+        location = "top";
+        lengthMode = "fill";
+        height = 35;
+        floating = true;
+        alignment = "left";
 
+        widgets = [
+          "org.kde.plasma.systemmonitor.cpu"
+          "org.kde.plasma.systemmonitor.memory"
+          "org.kde.plasma.systemmonitor.diskusage"
+          "org.kde.plasma.systemmonitor.net"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.notifications"
+        ];
+      }
+    ];
   };
 }
