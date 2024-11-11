638d8116ca2673f3ccbcd8ce9f045f045bb1f1b0
soriphoono <soriphoono@gmail.com>
2024-11-10 19:20:47 -0600

    Updated sddm theme

--- modules/nixos/desktop/services/sddm.nix
+++ modules/nixos/desktop/services/sddm.nix
@@ -11,10 +11,12 @@ in {
     environment.systemPackages = [
       (pkgs.where-is-my-sddm-theme.override {
         themeConfig.General = {
-          background = "${../../../../assets/wallpapers/kung-fu.jpg}";
-          backgroundMode = "fill";
+          backgroundFill = "#1e1e2e";
+          basicTextColor = "#cdd6f4";
+          passwordCursorColor = "#cdd6f4";
+          passwordInputBackground = "#1e1e2e";
+          passwordTextColor = "#cdd6f4";
         };
-        variants = [ "qt5" ];
       })
     ];
 
