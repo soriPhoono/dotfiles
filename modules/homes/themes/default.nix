26c069adc110bde0f425266a8d74b51690a58683
soriphoono <soriphoono@gmail.com>
2024-11-11 13:15:09 -0600

    Refactor catppuccin theme configuration and enhance default theme options

--- modules/homes/themes/default.nix
+++ modules/homes/themes/default.nix
@@ -1,1 +1,64 @@
-{ imports = [ ./catppuccin.nix ]; }
+{ inputs, lib, pkgs, config, ... }:
+let
+  cfg = config.themes;
+in
+{
+  imports = with inputs; [
+    stylix.homeManagerModules.stylix
+
+    ./catppuccin.nix
+  ];
+
+  options = {
+    themes = {
+      enable = lib.mkEnableOption "Enable themes system (Don't enable this manually unless you know what you're doing!)";
+    };
+  };
+
+  config = lib.mkIf cfg.enable
+    {
+      stylix = {
+        enable = true;
+
+        cursor = {
+          package = pkgs.catppuccin-cursors.mochaTeal;
+          size = 24;
+          name = "catppuccin-mocha-teal-cursors";
+        };
+
+        fonts = {
+          serif = {
+            package = pkgs.nerdfonts;
+            name = "AurulentSansM Nerd Font Propo";
+          };
+
+          sansSerif = {
+            package = pkgs.nerdfonts;
+            name = "AurulentSansM Nerd Font Propo";
+          };
+
+          monospace = {
+            package = pkgs.nerdfonts;
+            name = "JetBrainsMono Nerd Font Mono";
+          };
+
+          emoji = {
+            package = pkgs.noto-fonts-emoji;
+            name = "Noto Color Emoji";
+          };
+
+          sizes =
+            let
+              default_font = 14;
+              focus_font = 16;
+            in
+            {
+              applications = focus_font;
+              desktop = focus_font;
+              popups = default_font;
+              terminal = focus_font;
+            };
+        };
+      };
+    };
+}
