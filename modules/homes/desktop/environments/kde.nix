189966257284fd44a557adbc16f8045161c9495f
soriphoono <soriphoono@gmail.com>
2024-11-11 17:00:47 -0600

    Testing final iteration of bottom bar

--- modules/homes/desktop/environments/kde.nix
+++ modules/homes/desktop/environments/kde.nix
@@ -21,18 +21,12 @@ in {
         {
           location = "bottom";
           widgets = [
-            # Or you can configure the widgets by adding the widget-specific options for it.
-            # See modules/widgets for supported widgets and options for these widgets.
-            # For example:
             {
               kickoff = {
                 sortAlphabetically = true;
                 icon = "nix-snowflake-white";
               };
             }
-            # Adding configuration to the widgets can also for example be used to
-            # pin apps to the task-manager, which this example illustrates by
-            # pinning dolphin and konsole to the task-manager by default with widget-specific options.
             {
               iconTasks = {
                 launchers = [
@@ -41,9 +35,10 @@ in {
                 ];
               };
             }
+            "org.kde.plasma.showdesktop"
             # If no configuration is needed, specifying only the name of the
             # widget will add them with the default configuration.
-            "org.kde.plasma.marginsseparator"
+            # "org.kde.plasma.marginsseparator"
             # If you need configuration for your widget, instead of specifying the
             # the keys and values directly using the config attribute as shown
             # above, plasma-manager also provides some higher-level interfaces for
@@ -52,26 +47,26 @@ in {
             # of usage, one where we add a digital clock, setting 12h time and
             # first day of the week to Sunday and another adding a systray with
             # some modifications in which entries to show.
-            {
-              digitalClock = {
-                calendar.firstDayOfWeek = "sunday";
-                time.format = "12h";
-              };
-            }
-            {
-              systemTray.items = {
-                # We explicitly show bluetooth and battery
-                shown = [
-                  "org.kde.plasma.battery"
-                  "org.kde.plasma.bluetooth"
-                ];
-                # And explicitly hide networkmanagement and volume
-                hidden = [
-                  "org.kde.plasma.networkmanagement"
-                  "org.kde.plasma.volume"
-                ];
-              };
-            }
+            # {
+            #   digitalClock = {
+            #     calendar.firstDayOfWeek = "sunday";
+            #     time.format = "12h";
+            #   };
+            # }
+            # {
+            #   systemTray.items = {
+            #     # We explicitly show bluetooth and battery
+            #     shown = [
+            #       "org.kde.plasma.battery"
+            #       "org.kde.plasma.bluetooth"
+            #     ];
+            #     # And explicitly hide networkmanagement and volume
+            #     hidden = [
+            #       "org.kde.plasma.networkmanagement"
+            #       "org.kde.plasma.volume"
+            #     ];
+            #   };
+            # }
           ];
           alignment = "center";
           hiding = "autohide";
