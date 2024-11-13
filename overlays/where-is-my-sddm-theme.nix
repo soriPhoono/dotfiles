331ccdaf48f93737799928fd790ae00bd0e8aeb7
soriphoono <soriphoono@gmail.com>
2024-11-13 11:00:04 -0600

    Updated sddm theme

--- overlays/where-is-my-sddm-theme.nix
+++ overlays/where-is-my-sddm-theme.nix
@@ -1,16 +1,19 @@
 final: prev: {
   where-is-my-sddm-theme = prev.where-is-my-sddm-theme.override {
     themeConfig.General = {
-      hideCursor = true;
+      hideCursor = false;
       passwordInputWidth = 0.5;
       passwordInputRadius = 0.5;
       passwordFontSize = 48;
       passwordInputCursorVisible = false;
-      backgroundFill = "#1e1e2e";
+      background = ../assets/wallpapers/catppuccin-mountain.jpg;
+      blurRadius = 10;
       basicTextColor = "#cdd6f4";
       passwordCursorColor = "#cdd6f4";
-      passwordInputBackground = "#1e1e2e";
+      passwordInputBackground = "#313244";
       passwordTextColor = "#cdd6f4";
+      showSessionsByDefault = true;
+      showUsersByDefault = true;
     };
   };
 }
