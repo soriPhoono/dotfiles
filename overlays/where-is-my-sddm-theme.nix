f1f6320856410dd979c7b93ef44669525e71412f
soriphoono <soriphoono@gmail.com>
2024-11-13 16:18:39 -0600

    Updated sddm theme

--- overlays/where-is-my-sddm-theme.nix
+++ overlays/where-is-my-sddm-theme.nix
@@ -2,18 +2,19 @@ final: prev: {
   where-is-my-sddm-theme = prev.where-is-my-sddm-theme.override {
     themeConfig.General = {
       hideCursor = false;
-      passwordInputWidth = 0.5;
+      passwordInputWidth = 0.4;
       passwordInputRadius = 0.5;
-      passwordFontSize = 48;
+      passwordFontSize = 36;
       passwordInputCursorVisible = false;
+      passwordCharacter = "•";
       backgroundFill = "#1e1e2e";
       blurRadius = 10;
       basicTextColor = "#cdd6f4";
       passwordCursorColor = "#cdd6f4";
       passwordInputBackground = "#313244";
       passwordTextColor = "#cdd6f4";
-      # showSessionsByDefault = true;
       showUsersByDefault = true;
+      usersFontSize = 24;
     };
   };
 }
