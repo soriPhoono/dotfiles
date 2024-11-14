6be676e4960939da789cd6e75a806f4721fdd651
soriphoono <soriphoono@gmail.com>
2024-11-14 12:28:52 -0600

    Created gaming module and fixed overlays

--- modules/homes/userapps/feature_sets/gaming.nix
+++ modules/homes/userapps/feature_sets/gaming.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    (prismlauncher-qt5.override {
-      jdks = [
-        zulu8
-        zulu
-      ];
-    })
+{ lib, pkgs, config, ... }:
+let cfg = config.userapps.feature_sets.gaming;
+in {
+  options = {
+    userapps.feature_sets.gaming = {
+      enable = lib.mkEnableOption "Enable gaming programs";
+    };
+  };
 
-    path-of-building
-  ];
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      (prismlauncher.override {
+        jdks = [
+          zulu8
+          zulu
+        ];
+      })
+
+      path-of-building
+    ];
+  };
 }
