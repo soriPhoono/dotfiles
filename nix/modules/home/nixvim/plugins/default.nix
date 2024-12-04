c764a566a029db651915c385f6b0da69f0e09ebb
soriphoono <soriphoono@gmail.com>
2024-12-03 22:39:55 -0600

    Updated ui, and default plugins

--- nix/modules/home/nixvim/plugins/default.nix
+++ nix/modules/home/nixvim/plugins/default.nix
@@ -1,9 +1,26 @@
 { ... }: {
   imports = [
+    ./noice.nix
     ./lualine.nix
     ./neotree.nix
 
     ./cmp.nix
     ./lsp.nix
   ];
+
+  programs.nixvim.plugins = {
+    gitsigns.enable = true;
+    autoclose.enable = true;
+
+    toggleterm = {
+      enable = true;
+      settings.direction = "float";
+    };
+
+    which-key = {
+      enable = true;
+
+      settings.preset = "helix";
+    };
+  };
 }
--- nix/modules/home/nixvim/plugins/noice.nix
+++ nix/modules/home/nixvim/plugins/noice.nix
@@ -9,6 +9,14 @@
       enable = true;
 
       settings = {
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          inc_rename = true;
+          long_message_to_split = false;
+          lsp_doc_border = true;
+        };
+
         lsp.override = {
           "cmp.entry.get_documentation" = true;
           "vim.lsp.util.convert_input_to_markdown_lines" = true;
