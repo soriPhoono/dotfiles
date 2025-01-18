c744ae54cc6dc28e347676327091d63e97e2c93b
soriphoono <soriphoono@gmail.com>
2025-01-18 16:54:53 -0600

    Updated shell

--- modules/nixos/core/shell.nix
+++ modules/nixos/core/shell.nix
@@ -21,49 +21,10 @@ in {
       then pkgs.fish
       else pkgs.bashInteractive;
 
-    programs = let
-      rebuild_script = pkgs.writeShellApplication {
-        name = "rebuild.sh";
-
-        runtimeInputs = with pkgs; [
-          nix-index
-        ];
-
-        # TODO: check this
-        text = ''
-          select choice in switch boot cancel
-          do
-            case $choice in
-              switch)
-                sudo nixos-rebuild switch --flake .#${config.networking.hostName}
-                break
-                ;;
-              boot)
-                sudo nixos-rebuild boot --flake .#${config.networking.hostName}
-                break
-                ;;
-              cancel)
-                exit 0
-                ;;
-              *)
-                echo "Invalid selection"
-                ;;
-            esac
-          done
-
-          nix-index
-        '';
-      };
-    in {
+    programs = {
       dconf.enable = true;
 
-      fish = {
-        enable = cfg.shell == "fish";
-
-        shellAliases = {
-          rebuild = "${rebuild_script}/bin/rebuild.sh";
-        };
-      };
+      fish.enable = cfg.shell == "fish";
     };
   };
 }
