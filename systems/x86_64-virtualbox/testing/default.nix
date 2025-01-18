280c27efcd845918ce476fc224ebaa42fb46615d
soriphoono <soriphoono@gmail.com>
2025-01-18 13:16:24 -0600

    refactor: Remove system properties for x86_64-virtualbox in default.nix
    - Remove all properties for the `system` object in `systems/x86_64-virtualbox/testing/default.nix`

--- systems/x86_64-virtualbox/testing/default.nix
+++ systems/x86_64-virtualbox/testing/default.nix
@@ -1,12 +1,1 @@
-_: {
-  system = {
-    boot = {
-      enable = true;
-      plymouth.enable = true;
-    };
-
-    networking.enable = true;
-    pipewire.enable = true;
-    power.enable = true;
-  };
-}
+_: {}
