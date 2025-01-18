5cea4da1e5337510d5915ffceca59403fd7c46b5
soriphoono <soriphoono@gmail.com>
2025-01-18 11:43:54 -0600

    Updated shell hook with gptcommit

--- checks/pre-commit-checks/default.nix
+++ checks/pre-commit-checks/default.nix
@@ -7,6 +7,8 @@ inputs.pre-commit-hooks.lib.${system}.run {
   src = ../../.;
 
   hooks = {
+    gptcommit.enable = true;
+
     alejandra.enable = true;
     flake-checker.enable = true;
     statix.enable = true;
