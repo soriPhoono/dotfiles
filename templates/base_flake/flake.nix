a0a2cba2a5a6f9e345a7dc8ced6dae85e976f719
soriphoono <soriphoono@gmail.com>
2025-01-19 16:28:44 -0600

    feat: Refactor and update Nix files including base flake and pre-commit hooks
    - Create a new file `default.nix` in the `checks/pre-commit-checks` directory with specific structure and hooks
    - Add a new file `default.nix` in `templates/base_flake/shells/development` with the necessary imports and configurations
    - Update `flake.nix` file with various changes such as input removal, renaming attributes, and updating URLs

--- templates/base_flake/flake.nix
+++ templates/base_flake/flake.nix
@@ -2,62 +2,33 @@
   description = "Basic flake for a nix project";
 
   inputs = {
-    systems.url = "github:nix-systems/default";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = inputs @ {
     self,
-    systems,
     nixpkgs,
-    flake-parts,
     ...
-  }: let
-    systems = import systems;
-  in
-    flake-parts.mkFlake {inherit inputs;} {
-      inherit systems;
+  }:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
 
-      imports = with inputs; [
-        pre-commit-hooks.flakeModule
-        treefmt-nix.flakeModule
-      ];
+      src = ./.;
+      snowfall.namespace = "NAME HERE";
 
-      flake = {
+      outputs-builder = channels: {
+        formatter = channels.nixpkgs.alejandra;
       };
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-
-        treefmt = {
-          programs.alejandra.enable = true;
-        };
-
-        pre-commit.settings.hooks = {
-          gptcommit.enable = true;
-
-          alejandra.enable = true;
-          flake-checker.enable = true;
-          statix.enable = true;
-        };
-
-        devShells.default = pkgs.mkShell {
-          shellHook = ''
-            ${config.pre-commit.installationScript}
-
-            echo 1>&2 "Welcome to the base development shell!"
-          '';
-        };
+      alias = {
+        shells.default = "development";
       };
     };
 }
