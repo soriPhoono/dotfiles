97e19331a89bd2c84296b2a4a488abed05a68d8c
soriphoono <soriphoono@gmail.com>
2025-01-20 17:55:30 -0600

    feat: Refactor and optimize file modifications in default.nix
    - Increase the number of returned recordings from 10 to 100 in default.nix
    - Correct a typo in the name of the GitHub action
    - Refactor the initialization of octokit to a separate file
    - Implement an OpenAI API for completions
    - Adjust the numeric tolerance for test files
    - Add 2 tests for the inclusive string split function

--- modules/home/supporting/hyprland/default.nix
+++ modules/home/supporting/hyprland/default.nix
@@ -27,8 +27,38 @@ in {
 
         bind =
           [
+            "$mod, Q, killactive, "
+
+            "$mod, Tab, swapnext, "
+
+            "$mod, F, togglefloating, "
+            "$mod, P, pin, active"
+            "$mod, C, centerwindow, "
+
+            "$mod CTRL_SHIFT, Up, resizeactive, 0 -10"
+            "$mod CTRL_SHIFT, Left, resizeactive, -10 0"
+            "$mod CTRL_SHIFT, Right, resizeactive, 10 0"
+            "$mod CTRL_SHIFT, Down, resizeactive, 0 10"
+
             "$mod, Return, exec, ${pkgs.kitty}/bin/kitty"
           ]
+          ++ (builtins.concatLists (builtins.genList (
+              i: let
+                directions = [
+                  "Up"
+                  "Left"
+                  "Right"
+                  "Down"
+                ];
+
+                key = builtins.elemAt directions i;
+                direction = lib.toLower (lib.substring 0 1 key);
+              in [
+                "$mod, ${key}, movefocus, ${direction}"
+                "$mod CTRL, ${key}, movewindow, ${direction}"
+              ]
+            )
+            4))
           ++ (
             builtins.concatLists (builtins.genList (
                 i: let
