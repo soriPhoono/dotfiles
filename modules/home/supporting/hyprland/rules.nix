c6a20441288f172f6aa5686ddb24de4a6fcb576f
soriphoono <soriphoono@gmail.com>
2025-02-05 12:03:44 -0600

    feat: Improve workspace and window rules for "Smart gaps" and opacity 0.8
    - Add workspace rule for "Smart gaps"
    - Add window rules for "Smart gaps" and "opacity 0.8, class:(.*)"

--- modules/home/supporting/hyprland/rules.nix
+++ modules/home/supporting/hyprland/rules.nix
@@ -25,6 +25,7 @@ in {
     wayland.windowManager.hyprland.settings = {
       workspace =
         [
+          # Smart gaps
           "w[tv1], gapsout:0, gapsin:0"
           "f[1], gapsout:0, gapsin:0"
         ]
@@ -32,10 +33,13 @@ in {
 
       windowrulev2 =
         [
+          # Smart gaps
           "bordersize 0, floating:0, onworkspace:w[tv1]"
           "rounding 0, floating:0, onworkspace:w[tv1]"
           "bordersize 0, floating:0, onworkspace:f[1]"
           "rounding 0, floating:0, onworkspace:f[1]"
+
+          "opacity 0.8, class:(.*)"
         ]
         ++ cfg.windowRules;
     };
