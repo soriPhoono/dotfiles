330798b988aa77f0ed7e735bc985863a48b64bd2
soriphoono <soriphoono@gmail.com>
2025-02-05 14:27:29 -0600

    refactor: Refactor autostart configuration for hyprland window manager
    - Import `pkgs` for later use in `autostart.nix`
    - Add `exec-once` entry in `hyprland.settings` for `wayland.windowManager`

--- modules/home/hyprland/soriphoono/autostart.nix
+++ modules/home/hyprland/soriphoono/autostart.nix
@@ -1,11 +1,35 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
   cfg = config.hyprland.soriphoono;
 in {
-  config =
-    lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
+    wayland.windowManager.hyprland.settings = let
+      wallpaperScript = pkgs.writeShellApplication {
+        name = "wallpaper.sh";
+
+        runtimeInputs = with pkgs; [
+          swww
+        ];
+
+        text = ''
+          sleep 1
+
+          swww-daemon &
+
+          ${builtins.concatStringsSep "\n" (lib.mapAttrsToList (name: val:
+            if val == "regular"
+            then "swww img ../../../../assets/wallpapers/${name}"
+            else "") (builtins.readDir ../../../../assets/wallpapers))}
+        '';
+      };
+    in {
+      exec-once = [
+        "${wallpaperScript}/bin/wallpaper.sh"
+      ];
     };
+  };
 }
