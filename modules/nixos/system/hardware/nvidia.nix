0dd2f184b3817d7405dc8c9da15f33f7a6ca7756
soriphoono <soriphoono@gmail.com>
2025-02-04 21:49:56 -0600

    feat: Implement NVIDIA hardware configuration in NixOS
    - Add `nvidia.nix` file and define necessary options and configurations for NVIDIA hardware
    - Enable NVIDIA graphics, 32-bit support, power management, and dynamic boost
    - Configure NVIDIA Prime with Intel and AMD GPU bus IDs and enable offload feature
    - Include `nvtop` package in systemPackages
    - Set environment variables for NVIDIA acceleration if enabled

---
+++ modules/nixos/system/hardware/nvidia.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  pkgs,
+  config,
+  virtual,
+  ...
+}: let
+  cfg = config.system.hardware.nvidia;
+in {
+  options.system.hardware.nvidia = {
+    enable = lib.mkEnableOption "Enable NVIDIA GPU support";
+
+    acceleration = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable hardware acceleration features on NVIDIA GPUS";
+
+      default = true;
+    };
+  };
+
+  config = lib.mkIf (!virtual && cfg.enable) {
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+
+      nvidia = {
+        open = true;
+
+        powerManagement = {
+          enable = true;
+          finegrained = true;
+        };
+
+        dynamicBoost = {
+          enable = true;
+        };
+
+        prime = {
+          intelBusId = lib.mkIf config.system.hardware.intelgpu.integrated.enable "PCI:0:2:0";
+          amdgpuBusId = lib.mkIf config.system.hardware.amdgpu.integrated.enable "PCI:4:0:0";
+
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+        };
+      };
+    };
+
+    environment = {
+      systemPackages = with pkgs; [
+        nvtop
+      ];
+
+      variables = lib.mkIf (cfg.enable && cfg.acceleration) {
+        LIBVA_DRIVER_NAME = "nvidia";
+        VDPAU_DRIVER = "nvidia";
+      };
+    };
+  };
+}
