559ebab83b507ca4af37cc4d9c06f19325ababbe
soriphoono <soriphoono@gmail.com>
2025-02-06 14:00:51 -0600

    feat: Add configurations for Firefox and Kitty programs
    - Added default search engine configuration for Firefox profile
    - Added new search engines "Nix Packages" and "NixOS Wiki" with corresponding URLs, icons, and settings
    - Updated Firefox profile settings and added extensions
    - Enabled the `kitty` program and added fish integration
    - Set custom settings for `kitty` including cursor shape and trail
    - Added keybinding to execute `kitty` on key press
    - Added `kitty` program to the `noir` desktop environment

--- modules/home/desktop/noir/programs/firefox.nix
+++ modules/home/desktop/noir/programs/firefox.nix
@@ -11,6 +11,42 @@ in {
       enable = true;
 
       profiles.default = {
+        isDefault = true;
+
+        search = {
+          default = "DuckDuckGo";
+
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+
+            "NixOS Wiki" = {
+              urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
+              iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = ["@nw"];
+            };
+          };
+        };
+
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
         ];
--- modules/home/desktop/noir/programs/kitty.nix
+++ modules/home/desktop/noir/programs/kitty.nix
@@ -6,7 +6,16 @@
   cfg = config.desktop.noir;
 in {
   config = lib.mkIf cfg.enable {
-    programs.kitty.enable = true;
+    programs.kitty = {
+      enable = true;
+
+      shellIntegration.enableFishIntegration = config.core.shells.fish.enable;
+
+      settings = {
+        cursor_shape = "beam";
+        cursor_trail = 1;
+      };
+    };
 
     desktop.noir.extraBinds = [
       "$mod, Return, exec, kitty"
