99a68ac04ba8c2896b8e97e9e9fd52afbf7f641f
soriphoono <soriphoono@gmail.com>
2025-02-07 09:53:13 -0600

    feat: Update waybar statusBar settings and modules
    - Add new modules to the `statusBar` settings: `bluetooth`, `battery`, `custom/spacer`, `custom/separator`, `custom/session_controls`, and `hyprland/workspaces`
    - Update the format and icons for existing modules in the `statusBar` settings
    - Remove the `upower` module from the `modules-right` section
    - Add a `clock` module to the `statusBar` settings
    - Update CSS for the `window#waybar.statusBar` element in the style section

--- modules/home/desktop/noir/programs/waybar.nix
+++ modules/home/desktop/noir/programs/waybar.nix
@@ -11,16 +11,15 @@ in {
 
       settings = {
         statusBar = {
+          name = "statusBar";
+
           layer = "top";
 
           height = 20;
 
-          margin-top = 20;
-          margin-left = 20;
-          margin-right = 20;
-
           modules-left = [
             "custom/session_controls"
+            "custom/separator"
             "hyprland/workspaces"
           ];
 
@@ -28,9 +27,23 @@ in {
           ];
 
           modules-right = [
-            "upower"
+            "bluetooth"
+            "battery"
+            "custom/spacer"
             "clock"
           ];
+
+          "custom/spacer" = {
+            format = " ";
+
+            tooltip = false;
+          };
+
+          "custom/separator" = {
+            format = " | ";
+
+            tooltip = false;
+          };
 
           "custom/session_controls" = {
             format = "";
@@ -50,13 +63,6 @@ in {
               default = "󰑊";
               empty = "";
               urgent = "󰑊";
-
-              "1" = "";
-              "2" = "";
-              "3" = "󰈹";
-              "4" = "󰝚";
-              "5" = "";
-              "6" = "󰑋";
             };
 
             persistent-workspaces = {
@@ -64,12 +70,29 @@ in {
             };
           };
 
-          upower = {
-            format = " {percentage}";
+          bluetooth = {
+            format-on = "󰂯";
+            format-connected = "󰂱";
+            format-connected-battery = "󰂱 {device_battery_percentage}";
 
-            native-path = "BAT0";
-            icon-size = 15;
-            hide-if-empty = true;
+            tooltip-format-on = "{device_alias} {status}";
+            tooltip-format-connected = "{device_alias} {status}\n{device_enumerate}";
+          };
+
+          battery = {
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+
+            format = "{icon}";
+            format-discharging-critical = "{capacity}% {icon}";
+            format-icons = ["" "" "" "" ""];
+
+            tooltip-format = "{capacity}%\nTime till: {timeTo}";
+          };
+
+          clock = {
           };
         };
       };
@@ -77,8 +100,17 @@ in {
       style =
         # CSS
         ''
-          * {
-            background: transparent
+          window#waybar.statusBar {
+            background: transparent;
+          }
+
+          window#waybar.statusBar .modules-left {
+            background: #24273a;
+
+            border: 2px #6e738d;
+            border-right-style: solid;
+            border-bottom-style: solid;
+            border-radius: 0px 1rem 1rem 0px;
           }
         '';
 
