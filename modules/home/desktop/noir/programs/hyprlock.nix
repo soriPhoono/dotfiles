7b3851f6803c3ab76c734f970c87882809d6ca0d
soriphoono <soriphoono@gmail.com>
2025-02-07 23:40:46 -0600

    feat: Add new configuration options for hyprlock program
    - Added new configuration for the `hyprlock` program
    - Added `background` section with properties like `path`, `blur_passes`, and `blur_size`
    - Added new `input-field` section with properties like `size`, `position`, and `monitor`
    - Modified the `general` settings of the `hyprlock` program

--- modules/home/desktop/noir/programs/hyprlock.nix
+++ modules/home/desktop/noir/programs/hyprlock.nix
@@ -15,6 +15,30 @@ in {
           grace = 5;
           ignore_empty_input = true;
         };
+
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "eDP-1";
+            dots_center = true;
+            fade_on_empty = true;
+            font_color = "rgb(202, 211, 245)";
+            inner_color = "rgb(91, 96, 120)";
+            outer_color = "rgb(24, 25, 38)";
+            outline_thickness = 3;
+            placeholder_text = "<span foreground='#cad3f5'>Password...</span>";
+            shadow_passes = 2;
+          }
+        ];
       };
     };
   };
