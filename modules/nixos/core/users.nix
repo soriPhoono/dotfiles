deff0d39a8c7a2aa49781c84275ea3519f0a9b55
soriphoono <soriphoono@gmail.com>
2025-02-12 12:28:22 -0600

    Updated openssh access keys

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -36,10 +36,6 @@ in {
         inherit (cfg) shell;
 
         initialPassword = "password";
-
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgxxFcqHVwYhY0TjbsqByOYpmWXqzlVyGzpKjqS8mO7 soriphoono@gmail.com"
-        ];
       });
     };
 
