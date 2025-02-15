0b7d6dc72d08b106ced753bfc36cca91f49863ef
soriphoono <soriphoono@gmail.com>
2025-02-15 15:22:01 -0600

    Added desktop configuration

---
+++ homes/x86_64-linux/soriphoono@workstation/default.nix
@@ -0,0 +1,11 @@
+{
+  desktop.noir.enable = true;
+
+  userapps.enable = true;
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "HDMI-A-1, 1920x1080@144, 0x0, 1"
+    ];
+  };
+}
---
+++ systems/x86_64-linux/workstation/default.nix
@@ -0,0 +1,37 @@
+{
+  facter.reportPath = ../../../facter/desktop.json;
+
+  core = {
+    boot = {
+      plymouth.enable = true;
+    };
+
+    hardware = {
+      enable = true;
+      ssd.enable = true;
+      gpu = {
+        enable = true;
+        integrated.intel = {
+          enable = true;
+          device_id = ""; # TODO: fill this out
+        };
+        dedicated.amd.enable = true;
+      };
+    };
+
+    services = {
+      geoclue2.enable = true;
+    };
+
+    power.enable = true;
+    audio.enable = true;
+    bluetooth.enable = true;
+  };
+
+  desktop.noir.enable = true;
+
+  themes = {
+    enable = true;
+    catppuccin.enable = true;
+  };
+}
