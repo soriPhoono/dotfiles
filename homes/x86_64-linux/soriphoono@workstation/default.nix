2cb1f7fed2cd9c144599efae9a89f090dfc8e128
soriphoono <soriphoono@gmail.com>
2025-02-18 14:08:29 -0600

    Updated handling of gpu launch params

--- homes/x86_64-linux/soriphoono@workstation/default.nix
+++ homes/x86_64-linux/soriphoono@workstation/default.nix
@@ -4,6 +4,10 @@
   userapps.enable = true;
 
   wayland.windowManager.hyprland.settings = {
+    env = [
+      "AQ_DRM_DEVICES=/dev/dri/card2;/dev/dri/card1"
+    ];
+
     monitor = [
       "DP-4, 1920x1080@165, 0x0, 1"
       "DP-5, 1920x1080@144, 1920x0, 1"
