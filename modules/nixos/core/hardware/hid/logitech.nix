8c2b45de7a51ff69aa32a6dab7e0e79d52122a46
soriphoono <soriphoono@gmail.com>
2025-02-20 15:10:09 -0600

    xchore: Refactor configuration files for improved customization options
    - Removed outdated TODO comment for updating udev rules for Logitech powerplay charger and G502 mouse
    - Removed outdated udev rules for QMK keyboard
    - Added udev rule to disable autosuspend for specific USB device
    - Disabled `stylix.targets.hyprlock` in the `config` file
    - Enabled `programs.hyprlock` in the `config` file
    - Adjusted various settings in the `hyprlock` program's `config` file, including disabling loading bar, hiding cursor, setting grace period to 5 seconds, ignoring empty input, changing background image path, adjusting blur settings, and changing size and position of the input field
    - Removed placeholder text customization from the input field section in the `hyprlock` program's `config` file

--- modules/nixos/core/hardware/hid/logitech.nix
+++ modules/nixos/core/hardware/hid/logitech.nix
@@ -13,13 +13,8 @@ in {
       enableGraphical = true;
     };
 
-    # TODO: update udev rules for logitech powerplay charger and g502 mouse
     services.udev.extraRules = ''
-      # blacklist for usb autosuspend
-      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", ATTR{idProduct}=="9205", GOTO="logitech_power_rules"
-
-      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
-      LABEL="logitech_power_rules"
+      ACTION="add", SUBSYSTEM="usb", ATTR{idVendor}="05e3", ATTR{idProduct}="0608", ATTR{power/autosuspend}="-1"
     '';
   };
 }
--- modules/nixos/core/hardware/hid/qmk.nix
+++ modules/nixos/core/hardware/hid/qmk.nix
@@ -10,13 +10,8 @@ in {
   config = lib.mkIf cfg.enable {
     hardware.keyboard.qmk.enable = true;
 
-    # TODO: update udev rules for qmk keyboard
     services.udev.extraRules = ''
-      # blacklist for usb autosuspend
-      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", ATTR{idProduct}=="9205", GOTO="qmk_power_rules"
-
-      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
-      LABEL="qmk_power_rules"
+      ACTION="add", SUBSYSTEM="usb", ATTR{idVendor}="1462", ATTR{idProduct}="7e06", ATTR{power/autosuspend}="-1"
     '';
   };
 }
