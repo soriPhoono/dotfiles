8bc6d7b97f4952d4c42c1c47606eca827450b170
soriphoono <soriphoono@gmail.com>
2025-03-03 10:13:34 -0600

    Restored firefox, configuration options

--- modules/home/userapps/programs/firefox.nix
+++ modules/home/userapps/programs/firefox.nix
@@ -9,17 +9,90 @@ in {
   options.userapps.programs.firefox.enable = lib.mkEnableOption "Enable Firefox";
 
   config = lib.mkIf cfg.enable {
-    programs.librewolf = {
-      enable = true;
+    programs = {
+      firefox = {
+        enable = true;
 
-      policies = {
-        DisableTelementry = true;
-        DisplayBookmarksToolbar = "never";
+        profiles.default = {
+          isDefault = true;
+
+          search = {
+            default = "DuckDuckGo";
+
+            engines = {
+              "Nix Packages" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = ["@np"];
+              };
+
+              "Nix Options" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "type";
+                        value = "options";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = ["@no"];
+              };
+            };
+          };
+
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            privacy-badger
+            tabliss
+          ];
+
+          settings = {
+            extensions.autoDisableScopes = 0;
+          };
+        };
+
+        policies = {
+          DisableTelementry = true;
+          DisplayBookmarksToolbar = "never";
+        };
+      };
+
+      librewolf = {
+        enable = true;
+
+        policies = {
+          DisableTelementry = true;
+          DisplayBookmarksToolbar = "never";
+        };
       };
     };
 
     wayland.windowManager.hyprland.settings.bind = [
-      "$mod, B, exec, ${pkgs.librewolf}/bin/librewolf"
+      "$mod, B, exec, ${pkgs.firefox}/bin/firefox"
     ];
 
     services = {
