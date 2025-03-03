3aabb60059b280667027c3eee3fafda15d47f398
soriphoono <soriphoono@gmail.com>
2025-03-02 20:31:15 -0600

    feat: Update default.nix configuration
    - Add new `default.nix` file for x86_64-linux testbench
    - Set `facter.reportPath` to `../../../facter/testbench.json`
    - Enable various hardware features (android, bluetooth, monitors, ssd, integrated AMD GPU)
    - Enable networking features (wireless, openssh)
    - Enable services (geoclue2, pipewire)
    - Enable power suite
    - Enable `desktop.noir` option
    - Enable `catppuccin` theme

---
+++ systems/x86_64-linux/testbench/default.nix
@@ -0,0 +1,40 @@
+{
+  facter.reportPath = ../../../facter/testbench.json;
+
+  core = {
+    hardware = {
+      enable = true;
+      android.enable = true;
+      bluetooth.enable = true;
+      monitors.enable = true;
+      ssd.enable = true;
+
+      gpu = {
+        enable = true;
+        integrated.amd.enable = true;
+      };
+    };
+
+    networking = {
+      enable = true;
+      wireless.enable = true;
+      openssh.enable = true;
+    };
+
+    services = {
+      geoclue2.enable = true;
+      pipewire.enable = true;
+    };
+
+    suites = {
+      power.enable = true;
+    };
+  };
+
+  desktop.noir.enable = true;
+
+  themes = {
+    enable = true;
+    catppuccin.enable = true;
+  };
+}
