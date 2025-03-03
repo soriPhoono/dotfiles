17ca8bb31b191d928e7cefd72f6b1b8376460e44
soriphoono <soriphoono@gmail.com>
2025-03-03 10:34:31 -0600

    Added hotkey for file browser

--- homes/x86_64-linux/soriphoono/nvim/plugins/filetree.nix
+++ homes/x86_64-linux/soriphoono/nvim/plugins/filetree.nix
@@ -1,13 +1,23 @@
 {
-  programs.nvf.settings.vim.filetree = {
-    neo-tree = {
-      enable = true;
+  programs.nvf.settings.vim = {
+    keymaps = [
+      {
+        key = "\\";
+        mode = "n";
+        silent = true;
+        action = "<CMD>Neotree toggle<CR>";
+      }
+    ];
+    filetree = {
+      neo-tree = {
+        enable = true;
 
-      setupOpts = {
-        enable_cursor_hijack = true;
-        auto_clean_after_session_restore = true;
-        git_status_async = true;
-        hide_root_node = true;
+        setupOpts = {
+          enable_cursor_hijack = true;
+          auto_clean_after_session_restore = true;
+          git_status_async = true;
+          hide_root_node = true;
+        };
       };
     };
   };
