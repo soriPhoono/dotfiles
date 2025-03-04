d1d91ed856ce0ad9d5e5e6d49a77bdc556af852b
soriphoono <soriphoono@gmail.com>
2025-03-03 19:26:44 -0600

    chore: Refactor GPU option naming
    - Fix naming inconsistency in the `nvidia.nix` file
    - Rename the `mode` option to `laptopMode`

--- modules/nixos/core/hardware/gpu/dedicated/nvidia.nix
+++ modules/nixos/core/hardware/gpu/dedicated/nvidia.nix
@@ -8,11 +8,7 @@ in {
   options.core.hardware.gpu.dedicated.nvidia = {
     enable = lib.mkEnableOption "Enable NVIDIA GPU support";
 
-    mode = lib.mkOption {
-      type = lib.types.enum ["laptop" "desktop" "both"];
-      default = "both";
-      description = "Configure GPU mode for laptops and desktops";
-    };
+    laptopMode = lib.mkEnableOption "Enable laptop on-battery profile for system";
   };
 
   config = lib.mkIf (config.core.hardware.enable && config.core.hardware.gpu.enable && cfg.enable) {
@@ -21,6 +17,7 @@ in {
 
       powerManagement = {
         enable = true;
+        finegrained = true;
       };
 
       dynamicBoost.enable = true;
@@ -30,27 +27,20 @@ in {
         intelBusId = lib.mkIf config.core.hardware.gpu.integrated.intel.enable "PCI:0:2:0";
         amdgpuBusId = lib.mkIf config.core.hardware.gpu.integrated.amd.enable "PCI:4:0:0";
         nvidiaBusId = "PCI:1:0:0";
+
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
       };
     };
 
     services.xserver.videoDrivers = ["nvidia"];
 
     specialisation = {
-      "on-battery".configuration = lib.mkIf (cfg.mode == "both" || cfg.mode == "laptop") {
+      "on-ac".configuration = lib.mkIf cfg.laptopMode {
         system.nixos.tags = ["laptop-mode"];
-        hardware.nvidia = {
-          powerManagement.finegrained = true;
-          prime.offload = {
-            enable = true;
-            enableOffloadCmd = true;
-          };
-        };
-      };
-      "on-ac".configuration = lib.mkIf (cfg.mode == "both" || cfg.mode == "desktop") {
-        system.nixos.tags = ["desktop-mode"];
-        hardware.nvidia.prime = {
-          sync.enable = true;
-        };
+        hardware.nvidia.prime.sync.enable = true;
       };
     };
   };
