d559f05b8cf1d63b55f47bb710706bfcfdf1ccae
soriphoono <soriphoono@gmail.com>
2025-03-14 17:06:42 -0500

    Fixed bitwarden deployment file

--- modules/home/userapps/programs/bitwarden.nix
+++ modules/home/userapps/programs/bitwarden.nix
@@ -9,20 +9,18 @@ in {
   options.userapps.programs.bitwarden.enable = lib.mkEnableOption "Enable Bitwarden desktop and cli clients";
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      bitwarden-desktop
-    ];
+    home = {
+      packages = with pkgs; [
+        bitwarden-desktop
+      ];
 
-    home.sessionVariables = {
-      SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
+      sessionVariables = {
+        SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
+      };
     };
 
     programs.firefox.profiles.default.extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
       bitwarden
-    ];
-
-    core.impermanence.files = [
-      ".bitwarden-ssh-agent.sock"
     ];
   };
 }
