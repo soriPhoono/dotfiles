0e82c3bf9a04b1c9c9c8c2a1db72883dfeb3f6ae
soriphoono <soriphoono@protonmail.com>
2025-03-27 17:04:46 -0500

    Added stow

--- scripts/desktop/development.sh
+++ scripts/desktop/development.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+source ./scripts/util/default.sh
+
+info "Installing editors"
+
+install_packages neovim visual-studio-code-bin
+
+# Install neovim dependencies
--- scripts/desktop/user.sh
+++ scripts/desktop/user.sh
@@ -5,5 +5,4 @@ source ./scripts/util/default.sh
 
 install_packages stow fish starship
 
-install_packages zip unzip unrar nvtop btop eza fastfetch git
-
+install_packages zip unzip unrar nvtop btop eza fastfetch git bitwarden discord thunderbird
