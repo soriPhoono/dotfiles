78bd9eb76c7bd1840d45b0d2646a52a689c11611
soriphoono <soriphoono@gmail.com>
2025-03-29 18:43:06 -0500

    Removed nushell configurations, updated bootstrap scripts with extra applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,13 @@ stow .
 
 paru -S --needed --noconfirm fish starship btop \
   eza fastfetch zip unzip unrar nvtop bc yazi \
-  nodejs npm rustup ripgrep zellij \
+  nodejs npm rustup ripgrep zellij neovim \
+  visual-studio-code-bin onlyoffice-bin \
   bitwarden firefox profile-sync-daemon thunderbird \
-  obsidian discord signal-desktop element-desktop
+  obsidian discord signal-desktop element-desktop \
+  winetricks protontricks-git protonup gamemode \
+  gamescope steam lutris bottles prismlauncher gzdoom \
+  obs-studio obs-vkcapture obs-gstreamer gstreamer-vaapi
 
 chsh --shell /usr/bin/fish
 
