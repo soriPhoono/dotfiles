0024af929bc1feb14eb321e7bd78b9659c6c3cbe
soriphoono <soriphoono@gmail.com>
2025-03-29 01:16:33 -0500

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,21 @@
 
 echo "Dotfiles -- Sori Phoono"
 
-cd ~/.dotfiles
+cd ~/.dotfiles || exit
 
 git submodule init && git submodule update
 
+paru -S --needed --noconfirm stow
+
 stow .
+
+paru -S --needed --noconfirm fish starship btop \
+  eza fastfetch zip unzip unrar nvtop bc yazi \
+  nodejs npm rustup ripgrep zellij bitwarden \
+  firefox profile-sync-daemon discord obsidian
+
+chsh --shell /usr/bin/fish
+
+rustup default stable
+
+systemctl --user enable --now psd.service
