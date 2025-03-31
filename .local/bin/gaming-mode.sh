b00599012a094aa6b38f739450941725089b37a0
soriphoono <soriphoono@gmail.com>
2025-03-31 11:40:27 -0500

    Updated neovim and gaming mode script for desktopZ

--- .local/bin/gaming-mode.sh
+++ .local/bin/gaming-mode.sh
@@ -1,9 +1,18 @@
+monitor_mode=$1
+
 gnome-monitor-config set -LpM DP-1 -m 1920x1080@143.980
 
-if [ "$1" = "--fgc" ]; then
-  env DXVK_ASYNC=1 LD_PRELOAD="" gamemoderun gamescope -W 1920 -H 1080 -r 144 --adaptive-sync --mangoapp -fbe -- "${@:2}"
+if [ "$2" = "--fgc" ]; then
+  env DXVK_ASYNC=1 LD_PRELOAD="" gamemoderun gamescope -W 1920 -H 1080 -r 144 --adaptive-sync --mangoapp -fbe -- "${@:3}"
 else
-  env DXVK_ASYNC=1 LD_PRELOAD="" gamemoderun gamescope -W 1920 -H 1080 -r 144 --adaptive-sync --mangoapp -fbe -- "${@:1}"
+  env DXVK_ASYNC=1 LD_PRELOAD="" gamemoderun gamescope -W 1920 -H 1080 -r 144 --adaptive-sync --mangoapp -fbe -- "${@:2}"
 fi
 
-gnome-monitor-config set -LpM DP-1 -m 1920x1080@143.980 -LM HDMI-1 -x 1920 -m 1920x1080@74.986
+case $monitor_mode in
+triple)
+  gnome-monitor-config set -LM DP-5 -m 1920x1080@165.002 -LpM DP-1 -x 1920 -m 1920x1080@143.980 -LM HDMI-1 -x 3840 -m 1920x1080@74.986
+  ;;
+double)
+  gnome-monitor-config set -LpM DP-1 -m 1920x1080@143.980 -LM HDMI-1 -x 1920 -m 1920x1080@74.986
+  ;;
+esac
