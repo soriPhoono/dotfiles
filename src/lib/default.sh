21f473ecdbbad75e6f05d3937cb37730935b1bbc
soriphoono <soriphoono@protonmail.com>
2025-04-10 20:02:20 -0500

    Edited for next to last update

--- src/lib/default.sh
+++ src/lib/default.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo cp -r ./root/* /
-
 if [[ "$REPLY" =~ [yY] ]]; then
   paru -S --needed --noconfirm winetricks protontricks-git protonup gamemode \
     gamescope steam lutris bottles prismlauncher gzdoom \
