489ca9317e8e184b77e54f193a0a0b6902d0aec6
soriphoono <soriphoono@protonmail.com>
2025-04-17 12:13:09 -0500

    Updated bootstrap script

---
+++ .config/yadm/src/gaming.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+paru -S --noconfirm --needed \
+  dxvk-async-git vkd3d lib32-vkd3d \
+  mangohud gamescope gamemode \
+  steam lutris bottles prism-launcher gzdoom tome4 \
+  winetricks protontricks \
