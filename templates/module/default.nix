212417b5a5083f34e3b78a636c38576bc4913652
soriphoono <soriphoono@protonmail.com>
2025-05-10 13:21:28 -0500

    See previous

---
+++ templates/module/default.nix
@@ -0,0 +1,15 @@
+{inputs, lib, pkgs, config, namespace, ...}:
+let
+  path = "${namespace}.Your path here";
+  cfg = config.${path};
+in {
+  options.${path} = {
+    enable = lib.mkEnableOption "Enable ${path} module";
+  };
+
+  config = lib.mkIf cfg.enable {
+    warnings = [
+      "TODO: ${path} module"
+    ];
+  };
+}
