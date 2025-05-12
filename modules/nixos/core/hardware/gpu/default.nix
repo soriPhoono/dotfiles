df816f4cfd21c2bed0e706185231fcdd13a84a23
soriphoono <soriphoono@protonmail.com>
2025-05-12 15:49:38 -0500

    Updated hardware imports for gpu module

--- modules/nixos/core/hardware/gpu/default.nix
+++ modules/nixos/core/hardware/gpu/default.nix
@@ -6,6 +6,13 @@
 }: let
   cfg = config.${namespace}.core.hardware.gpu;
 in {
+  imports = [
+    ./dedicated/amd.nix
+    ./dedicated/nvidia.nix
+    ./integrated/amd.nix
+    ./integrated/intel.nix
+  ];
+
   options.${namespace}.core.hardware.gpu = {
     enable = lib.mkEnableOption "Enable graphics driver features";
   };
