e8169ef1ebeb97eff4105d47cd52d49a877814e5
soriphoono <soriphoono@gmail.com>
2025-06-03 22:24:41 -0500

    Updated zephyrus

--- systems/x86_64-linux/zephyrus/default.nix
+++ systems/x86_64-linux/zephyrus/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [
     ./disko.nix
   ];
@@ -28,6 +28,16 @@
       tailscale = {
         enable = true;
         tn_name = "xerus-augmented.ts.net";
+      };
+    };
+
+    users = {
+      soriphoono = {
+        email = "soriphoono@gmail.com";
+        hashedPassword = "$6$x7n.SUTMtInzs2l4$Ew3Zu3Mkc4zvuH8STaVpwIv59UX9rmUV7I7bmWyTRjomM7QRn0Jt/Pl/JN./IqTrXqEe8nIYB43m1nLI2Un211";
+        admin = true;
+        shell = pkgs.fish;
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgxxFcqHVwYhY0TjbsqByOYpmWXqzlVyGzpKjqS8mO7";
       };
     };
   };
