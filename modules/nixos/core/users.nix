ce51fce4beb13bc20c84c9c978c276b878c21076
soriphoono <soriphoono@gmail.com>
2025-08-26 17:38:27 -0500

    Updated ssh key for spookyskelly

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -72,7 +72,7 @@ in {
       mutableUsers = false;
 
       extraUsers = builtins.mapAttrs (_: user: let
-        adminKeys = if !user.admin (lib.mapAttrsToList (_: admin: admin.publicKey) (lib.filterAttrs (_: user: user.admin) cfg.users)) else [];
+        adminKeys = if !user.admin then (lib.mapAttrsToList (_: admin: admin.publicKey) (lib.filterAttrs (_: user: user.admin) cfg.users)) else [];
       in {
         inherit (user) hashedPassword extraGroups shell;
 
