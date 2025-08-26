341ce2f9c971985de435b1e3207c5a412414d493
soriphoono <soriphoono@gmail.com>
2025-08-25 21:16:00 -0500

    Refactoring

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -6,6 +6,11 @@
 }: let
   cfg = config.desktop;
 in {
+  imports = [
+    ./environments/gnome.nix
+    ./environments/kde.nix
+  ];
+
   options.desktop = {
     enable = lib.mkEnableOption "Enable core desktop configurations";
   };
@@ -14,5 +19,22 @@ in {
     fonts.packages = with pkgs; [
       carlito
     ];
+
+    services = {
+      pulseaudio.enable = false;
+
+      pipewire = {
+        enable = true;
+        audio.enable = true;
+        pulse.enable = true;
+        jack.enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+      };
+    };
+
+    security.rtkit.enable = true;
   };
 }
