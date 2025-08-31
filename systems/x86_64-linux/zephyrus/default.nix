be7f99dcf6562f9975da39f015d0567f8857b9db
soriphoono <soriphoono@gmail.com>
2025-08-31 12:50:00 -0500

    Updated spookyskelly shell

--- systems/x86_64-linux/zephyrus/default.nix
+++ systems/x86_64-linux/zephyrus/default.nix
@@ -35,14 +35,12 @@
       soriphoono = {
         hashedPassword = "$6$x7n.SUTMtInzs2l4$Ew3Zu3Mkc4zvuH8STaVpwIv59UX9rmUV7I7bmWyTRjomM7QRn0Jt/Pl/JN./IqTrXqEe8nIYB43m1nLI2Un211";
         admin = true;
-        extraGroups = [
-          "adbusers"
-        ];
         shell = pkgs.fish;
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgxxFcqHVwYhY0TjbsqByOYpmWXqzlVyGzpKjqS8mO7";
       };
       spookyskelly = {
         hashedPassword = "$y$j9T$2ClMbK8AGR2tDvxqsQi7N/$VoJZOzxRwbq6GZ9zBR0E2gq0GsZ3Oo27RcjCyG/Gct5";
+        shell = pkgs.fish;
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMKr5+AriekN7pqmK2bIAWbYqulnxTHL56+dB9Od6vs";
       };
     };
