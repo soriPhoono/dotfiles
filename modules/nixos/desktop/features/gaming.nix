7cc9d4ce97707477e944fc891f4cff43e134eda9
soriphoono <soriphoono@gmail.com>
2025-09-04 13:03:26 -0500

    Updated with vr tech for all platforms, refining...

--- modules/nixos/desktop/features/gaming.nix
+++ modules/nixos/desktop/features/gaming.nix
@@ -13,7 +13,6 @@ in {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       lutris
-      bottles
 
       protonplus
 
@@ -42,5 +41,57 @@ in {
         protontricks.enable = true;
       };
     };
+
+    services.wivrn = {
+      enable = true;
+      defaultRuntime = true;
+      highPriority = true;
+      autoStart = true;
+      steam.importOXRRuntimes = true;
+      # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
+      config = {
+        enable = true;
+        json = {
+          # 1.0x foveation scaling
+          scale = 1.0;
+          # 100 Mb/s
+          bitrate = 100000000;
+          encoders = [
+            {
+              encoder = "vaapi";
+              codec = "h265";
+              # 1.0 x 1.0 scaling
+              width = 1.0;
+              height = 1.0;
+              offset_x = 0.0;
+              offset_y = 0.0;
+            }
+          ];
+        };
+      };
+    };
+
+    # home-manager.users = 
+    #   builtins.mapAttrs (user: _: {
+    #     xdg.configFile = {
+    #       "openvr/openvrpaths.vrpath".text = ''
+    #         {
+    #           "config": [
+    #             "${config.home-manager.users.${user}.xdg.dataHome}/Steam/config"
+    #           ],
+    #           "external_drivers": null,
+    #           "jsonid": "vrpathreg",
+    #           "log": [
+    #             "${config.home-manager.users.${user}.xdg.dataHome}/Steam/logs"
+    #           ],
+    #           "runtime": [
+    #             "${pkgs.opencomposite}/lib/opencomposite",
+    #           ],
+    #           "version": 1
+    #         }
+    #       '';
+    #     };
+    #   })
+    #   config.core.users;
   };
 }
