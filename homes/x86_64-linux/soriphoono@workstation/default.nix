91462049989346893e798613c21f7da04e721ddf
soriphoono <soriphoono@gmail.com>
2025-11-07 18:02:41 -0600

    Updated soriphoono workstation environment with extra ssh key declaration

--- homes/x86_64-linux/soriphoono@workstation/default.nix
+++ homes/x86_64-linux/soriphoono@workstation/default.nix
@@ -17,7 +17,8 @@
       userEmail = "soriphoono@gmail.com";
       projectsDir = "/run/media/soriphoono/Projects";
       extraIdentities = {
-        Work = {
+        work_key = {
+          directory = "Work";
           name = "xrezdev11";
           email = "xrezdev11@gmail.com";
           signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAxax8ouXfptDoQkw4C0FgA4USyS8U6UZu76RRE2VtI";
