adaa6289cd3ad3d939fdfd741127514705a2f830
soriphoono <soriphoono@gmail.com>
2025-11-12 10:29:11 -0600

    Updated flake, cleaned up hosting module

--- modules/nixos/hosting/default.nix
+++ modules/nixos/hosting/default.nix
@@ -9,41 +9,22 @@ in
   with lib; {
     options.docker = {
       enable = mkEnableOption "Enable Docker support";
-
-      mode = mkOption {
-        type = types.enum ["standalone" "swarm-manager" "swarm-submanager" "swarm-worker"];
-        default = "standalone";
-        description = ''
-          Set the Docker mode.
-        '';
-      };
     };
 
-    config = mkIf cfg.enable (mkMerge [
-      {
-        virtualisation.docker = {
-          enable = true;
-          autoPrune.enable = true;
-        };
+    config = mkIf cfg.enable {
+      virtualisation.docker = {
+        enable = true;
+        autoPrune.enable = true;
+      };
 
-        users.extraUsers =
-          builtins.mapAttrs (name: user: {
-              extraGroups = [
-                "docker"
-              ];
-            })
-            (filterAttrs
-              (name: content: content.admin)
-              config.core.users);
-      }
-      (mkIf (cfg.mode == "swarm-manager") {
-
-      })
-      (mkIf (cfg.mode == "swarm-submanager") {
-
-      })
-      (mkIf (cfg.mode == "swarm-worker") {
-
-      })
-    ]);
+      users.extraUsers =
+        builtins.mapAttrs (name: user: {
+          extraGroups = [
+            "docker"
+          ];
+        })
+        (filterAttrs
+          (name: content: content.admin)
+          config.core.users);
+    };
   }
