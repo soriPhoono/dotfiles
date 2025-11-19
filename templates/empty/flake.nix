b2a01e5a85501ee2cb0740c52f57893118e8dca5
soriphoono <soriphoono@gmail.com>
2025-11-19 14:50:16 -0600

    Cleaned up templates

--- templates/empty/flake.nix
+++ templates/empty/flake.nix
@@ -2,23 +2,42 @@
   description = "Empty flake with basic devshell";
 
   inputs = {
+    systems.url = "github:nix-systems/default";
+
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    actions-nix.url = "github:nialov/actions.nix";
+    devenv.url = "github:cachix/devenv";
+    agenix-shell.url = "github:aciceri/agenix-shell";
   };
 
-  outputs = {
+  outputs = inputs @ {
+    systems,
     nixpkgs,
-    flake-utils,
+    flake-parts,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      formatter = pkgs.alejandra;
+    flake-parts.lib.mkFlake {inherit inputs;} (top @ {
+      config,
+      withSystem,
+      moduleWithSystem,
+      ...
+    }: {
+      inherit systems;
 
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-        ];
+      imports = with inputs; [
+        actions-nix.flakeModules.default
+        devenv.flakeModule
+        agenix-shell.flakeModules.default
+      ];
+      flake = {
+      };
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
       };
     });
 }
