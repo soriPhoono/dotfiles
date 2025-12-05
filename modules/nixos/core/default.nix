c6d4da05b9c766c916a81050f40c28df88b6c0c2
soriphoono <soriphoono@gmail.com>
2025-12-04 19:19:39 -0500

    Updated console

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -1,10 +1,24 @@
-{config, ...}: {
+{pkgs, config, ...}: {
   imports = [
     ./boot.nix
     ./nixconf.nix
     ./secrets.nix
     ./users.nix
   ];
+
+  console = {
+    keyMap = "us";
+    packages = with pkgs; [
+      terminus_font
+    ];
+    font = lib.mkDefault "Lat2-Terminus16";
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    consoleKeyMap = "us";
+    consoleFont = "Lat2-Terminus16";
+  };
 
   programs = {
     nix-ld.enable = true;
