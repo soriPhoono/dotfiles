c665bf792211fceb452c8cf5bc791e812a17eb92
soriphoono <soriphoono@gmail.com>
2025-12-05 13:45:39 -0500

    Updated group membership for soriphoono user

--- systems/x86_64-linux/adams/default.nix
+++ systems/x86_64-linux/adams/default.nix
@@ -7,7 +7,7 @@
   boot.growPartition = true;
 
   networking.firewall.allowedTCPPorts = [47984 47989 48010];
-  networking.firewall.allowedUDPPorts = [47998 47999 48100 48200];
+  networking.firewall.allowedUDPPorts = [47998 47999 48000 48100 48200];
 
   services.udev.extraRules = ''
     # Allows Wolf to acces /dev/uinput
