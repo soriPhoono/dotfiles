fd4929587d1851913ce11aa908a4b4ca23195da6
soriphoono <soriphoono@gmail.com>
2025-12-05 23:54:39 -0500

    Updated adams secret vault

--- homes/x86_64-linux/soriphoono@adams/default.nix
+++ homes/x86_64-linux/soriphoono@adams/default.nix
@@ -1,5 +1,10 @@
 {
   core = {
+    secrets = {
+      enable = true;
+      defaultSopsFile = ./secrets.yaml;
+    };
+
     git = {
       userName = "soriphoono";
       userEmail = "soriphoono@gmail.com";
