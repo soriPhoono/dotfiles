79227af0b4c42b885c49cacc8e6b58e823315cd1
soriphoono <soriphoono@gmail.com>
2025-12-06 15:12:20 -0500

    Fix quotes in extraOptions for wolf-server container configuration

--- systems/x86_64-linux/moonlight/default.nix
+++ systems/x86_64-linux/moonlight/default.nix
@@ -47,7 +47,7 @@
           "/dev:/dev:rw"
           "/run/udev:/run/udev:rw"
         ];
-        extraOptions = ["--device-cgroup-rule=\"c 13:* rmw\""];
+        extraOptions = ["--device-cgroup-rule='c 13:* rmw'"];
         devices = [
           "/dev/dri"
           "/dev/uinput"
