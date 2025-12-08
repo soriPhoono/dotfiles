ed8adbba3811b777ef3b909d5cd80542da6f8869
soriphoono <soriphoono@gmail.com>
2025-12-07 21:06:55 -0500

    Updated vms

---
+++ homes/x86_64-linux/soriphoono@moonlight-test/default.nix
@@ -0,0 +1,13 @@
+{
+  core = {
+    secrets = {
+      enable = true;
+      defaultSopsFile = ./secrets.yaml;
+    };
+
+    git = {
+      userName = "soriphoono";
+      userEmail = "soriphoono@gmail.com";
+    };
+  };
+}
