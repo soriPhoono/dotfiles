64f8eaea317c16261d2916cf124180c322d0a201
soriphoono <soriphoono@gmail.com>
2025-12-17 23:32:41 -0500

    Updated with testbench systems for desktop/system level changes

--- modules/nixos/desktop/features/gaming.nix
+++ modules/nixos/desktop/features/gaming.nix
@@ -14,63 +14,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      lutris
-
-      protonup-qt
-
-      mangohud
-
-      prismlauncher
-      gzdoom
+      moonlight-qt
     ];
-
-    programs = {
-      # Gamemode for advanced performance tuning
-      gamemode = {
-        enable = true;
-        enableRenice = true;
-      };
-
-      # Desktop steam on desktop mode compliant systems
-      steam = {
-        enable = true;
-
-        extest.enable = true;
-
-        remotePlay.openFirewall = true;
-        localNetworkGameTransfers.openFirewall = true;
-
-        protontricks.enable = true;
-      };
-    };
-
-    services.wivrn = lib.mkIf cfg.vr.enable {
-      enable = true;
-      defaultRuntime = true;
-      highPriority = true;
-      autoStart = true;
-      steam.importOXRRuntimes = true;
-      # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
-      config = {
-        enable = true;
-        json = {
-          # 1.0x foveation scaling
-          scale = 1.0;
-          # 100 Mb/s
-          bitrate = 100000000;
-          encoders = [
-            {
-              encoder = "vaapi";
-              codec = "h265";
-              # 1.0 x 1.0 scaling
-              width = 1.0;
-              height = 1.0;
-              offset_x = 0.0;
-              offset_y = 0.0;
-            }
-          ];
-        };
-      };
-    };
   };
 }
---
+++ systems/x86_64-linux/testbench/default.nix
@@ -0,0 +1,36 @@
+{pkgs, ...}: {
+  imports = [
+    ./disko.nix
+  ];
+
+  core = {
+    boot.enable = true;
+
+    hardware = {
+      enable = true;
+      reportPath = ./facter.json;
+    };
+
+    networking = {
+      network-manager.enable = true;
+      tailscale.enable = true;
+    };
+
+    users = {
+      soriphoono = {
+        hashedPassword = "$6$x7n.SUTMtInzs2l4$Ew3Zu3Mkc4zvuH8STaVpwIv59UX9rmUV7I7bmWyTRjomM7QRn0Jt/Pl/JN./IqTrXqEe8nIYB43m1nLI2Un211";
+        admin = true;
+        shell = pkgs.fish;
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgxxFcqHVwYhY0TjbsqByOYpmWXqzlVyGzpKjqS8mO7";
+      };
+    };
+  };
+
+  desktop = {
+    environments.kde.enable = true;
+    features = {
+      virtualisation.enable = true;
+      gaming.enable = true;
+    };
+  };
+}
