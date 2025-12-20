e574ce8d0452502b7f3401ceb104a0444a930b35
soriphoono <soriphoono@gmail.com>
2025-12-19 22:06:05 -0600

    Updated hyprland display manager

--- modules/nixos/desktop/managers/hyprland.nix
+++ modules/nixos/desktop/managers/hyprland.nix
@@ -5,14 +5,27 @@
   ...
 }: let
   cfg = config.desktop.environments.hyprland;
-in {
+in with lib; {
   options.desktop.environments.hyprland = {
-    enable = lib.mkEnableOption "Enable hyprland desktop environment base.";
+    enable = mkEnableOption "Enable hyprland desktop environment base.";
   };
 
-  config = lib.mkIf cfg.enable {
-    desktop.programs.uwsm = {
-      enable = true;
+  config = mkIf cfg.enable {
+    desktop.programs = {
+      sddm = mkIf (!config.programs.sddm.enable) {
+        enable = true;
+        theme = {
+          package = (pkgs.catppuccin-sddm.override {
+            flavor = "frappe";
+            accent = "teal";
+            background = "${lib.${namespace}.wallpaper "beach-path.jpg"}";
+            loginBackground = true;
+          });
+          name = "catppuccin-frappe-teal";
+        };
+      };
+
+      uwsm.enable = true;
     };
 
     programs.hyprland = {
